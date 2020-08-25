d26be08846d99edb1e58312623181c9430c66ede
python9066 <python9066@gmail.com>
2020-08-25 21:56:59 +0100

    removing stuff

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,11 +30,6 @@ class HomeController extends Controller
     public function party()
     {
 
-        $date = new DateTime();
-        $date->modify("+20 minutes");
-        $code = "4f8f8ad2b29e44cdb13f2f6d0f7d482d:QVyq0lie3rpCnGXLUBuV1JWr6rHh9z0RQDxWg6aI";
-        $code = base64_encode($code);
-        dd($code);
-        return view('test', compact('date','code'));
+
     }
 }
