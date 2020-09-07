6a3bee6882854c1272f36d341bdba0d1405aaf71
python9066 <python9066@gmail.com>
2020-09-07 22:32:47 +0100

    quick commit

--- routes/web.php
+++ routes/web.php
@@ -51,17 +51,15 @@ Route::get('/helper2', function () {
 
 Route::get('/367448c2da9ee714f64d0bce9dfd219fabf03dbccb1948969afea0d814c7e8d144/66cedf66cf26e0061eb2ca8ea6472c0c169f66', function () {
    Artisan::call('update:notifications');
-   return Helper::displayName();
 });
 
 Route::get('/367448c2da9ee714f64d0bce9dfd219fabf8969afea0d814c7e8d144/66cedf66cf2a6472c0c169f66', function () {
     Artisan::call('update:timers');
-    return Helper::displayName();
  });
 
  Route::get('/3714f64d0bce9dfd219fabf03dbccb1948969afea0d814c7e8d144/66cedf66cf26e006172c0c169f66', function () {
     Artisan::call('update:alliances');
-    return Helper::displayName();
+
  });
 
 Route::get('/corn/test', function () {
