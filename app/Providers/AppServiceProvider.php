1c22fddf7e2b48bedc32639ed1f1b39ba5c134c3
python9066 <python9066@gmail.com>
2020-09-07 02:35:06 +0100

    quick commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \Illuminate\Support\Facades\URL::forceScheme('https');
+
+        if($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
         $socialite = $this->app->make('Laravel\Socialite\Contracts\Factory');
         $socialite->extend(
         'gice',
