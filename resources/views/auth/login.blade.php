8064b80926fbc4b28a4ae7a4ad98219a169f0f71
python9066 <python9066@gmail.com>
2020-09-08 00:39:31 +0100

    quick commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header align-middle">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <div>When you login for the first time ever, it will spit an error.  Just refresh and all will be fine.  This only ever happens once.</div>
+                    <div>When you login for the first time ever, it will spit an error.  Just refresh and all will be fine.  This only ever happens once. (I have no idea why)</div>
                         <a class="btn btn-primary" href="/oauth/login" role="button">LOGIN</a>
                 </div>
             </div>
