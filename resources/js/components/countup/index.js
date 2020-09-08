f1295afe7a3f5bad497244e88a7ef285601d370f
python9066 <python9066@gmail.com>
2020-09-08 01:30:16 +0100

    quick commit

--- resources/js/components/countup/index.js
+++ resources/js/components/countup/index.js
@@ -333,6 +333,7 @@ const VueCountUptimer = {
             if (status === 1) {
               this.$set(this, 'tips', true);
               this.count = Math.max(0, startCount);
+              this.$emit('bla', this.status)
             }
 
             if (status === 2) {
