af0beebabf44b120b5b89921993c2893a3e6fa10
python9066 <python9066@gmail.com>
2020-09-08 15:59:32 +0100

    quick commit

---
+++ app/Http/Controllers/CampaginController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use utils\Helper\Helper;
+
+class CampaginController extends Controller
+{
+    public function updateCampagins()
+    {
+        $status = Helper::checkeve();
+        if ($status == 1){
+
+
+
+        }
+    }
+}
---
+++ utils/Campaginhelper.php
@@ -0,0 +1,3 @@
+<?php
+
+namespace utils\Campaginhelper;
