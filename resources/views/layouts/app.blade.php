40d892cb0d3d16c261ab4b3e1e6b73e98e55aae1
python9066 <python9066@gmail.com>
2020-09-08 12:14:43 +0100

    quick commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="{!! asset('svg/logo.svg') !!}"/>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
