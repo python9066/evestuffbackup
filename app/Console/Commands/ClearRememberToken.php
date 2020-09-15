ba902c6fa85ba0a1d2065a269f4f2d97e45f272f
python9066 <python9066@gmail.com>
2020-09-15 22:31:20 +0100

    quick commit

--- app/Console/Commands/ClearRememberToken.php
+++ app/Console/Commands/ClearRememberToken.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class ClearRememberToken.php extends Command
+class ClearRememberToken extends Command
 {
     /**
      * The name and signature of the console command.
