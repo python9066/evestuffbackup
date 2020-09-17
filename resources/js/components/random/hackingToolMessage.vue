41125618ab9fe82598280466afdea6fe76108598
python9066 <python9066@gmail.com>
2020-09-17 13:38:53 +0100

    quick commit

--- resources/js/components/random/hackingToolMessage.vue
+++ resources/js/components/random/hackingToolMessage.vue
@@ -8,7 +8,8 @@
       :icon = false
       :elevation = 24
     >
-      This whole page is very work in progress so things may not work fully.  Contact <strong> JohnMonty</strong> on Jabber you see anything 100% broken to the point its blowing up.
+     <p> This whole page is very work in progress so things may not work fully.  Contact <strong> JohnMonty</strong> on Jabber you see anything 100% broken to the point its blowing up.</p>
+     <p> If you want play around feel free.  Take to assign yourself to a node, you <strong>HAVE TO</strong> press the "ADD CHAR" button and pick<strong> Hacker </strong>as your role.</p>
     </v-alert>
   </div>
 </template>
