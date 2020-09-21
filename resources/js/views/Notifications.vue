821128fd4c03fc45841b667a1263290e0e642f4f
python9066 <python9066@gmail.com>
2020-09-21 03:06:39 +0100

    quick commit

--- resources/js/views/Notifications.vue
+++ resources/js/views/Notifications.vue
@@ -385,7 +385,7 @@
                 </td>
             </template>
             <template v-slot:item.user_name="{ item }" >
-                <p class ="d-flex justify-center"> {{item.user_name}}</p>
+                <p class ="d-flex align-center"> {{item.user_name}}</p>
             </template>
         </v-data-table>
         <v-snackbar v-model="snack" :timeout="3000" :color="snackColor">
