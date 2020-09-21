79c3109414756a94798833b4782798ee9b8b1150
python9066 <python9066@gmail.com>
2020-09-22 00:24:02 +0100

    quick commit

--- resources/js/views/Campaigns.vue
+++ resources/js/views/Campaigns.vue
@@ -405,7 +405,7 @@ export default {
         fixStartTime(item){
 
            var time = moment(item.start, "YYYY-MM-DD hh:mm:ss")
-           utc = moment.utc(time).format("YYYY-MM-DD hh:mm:ss")
+           var utc = moment.utc(time).format("YYYY-MM-DD hh:mm:ss")
            console.log(utc + " UTC");
         },
 
