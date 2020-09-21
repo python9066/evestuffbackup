4eb3180a3f071775f6d54ba0e5d4447653bbea56
python9066 <python9066@gmail.com>
2020-09-21 01:44:05 +0100

    quick commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,9 +29,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->everyMinute();
-        $schedule->command('update:campaigns')->everyMinute()->withoutOverlapping();
-        $schedule->command('update:notifications')->everyTwoMinutes()->withoutOverlapping();
-        $schedule->command('update:timers')->hourly()->withoutOverlapping();
+        $schedule->command('update:campaigns')->everyMinute()->unlessBetween('11:00', '11:20')->withoutOverlapping();
+        $schedule->command('update:notifications')->everyTwoMinutes()->unlessBetween('11:00', '11:20')->withoutOverlapping();
+        $schedule->command('update:timers')->hourly()->unlessBetween('11:00', '11:20')->withoutOverlapping();
         $schedule->command('update:alliances')->twiceDaily(9,22)->withoutOverlapping();
         $schedule->command('clear:remembertoken')->twiceDaily(9,21)->withoutOverlapping();
     }
