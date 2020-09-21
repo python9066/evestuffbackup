1da03fc29258c56c48a57557d5bd9f4e269a662e
python9066 <python9066@gmail.com>
2020-09-21 01:22:58 +0100

    quick commit

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -23,6 +23,7 @@ class NotificationController extends Controller
             $type = "note";
             Helper::authcheck();
             $data = Helper::authpull($type);
+            DD($data);
             $flag = Notifications::update($data);
             // dd($flag);
             if ($flag == 1) {
