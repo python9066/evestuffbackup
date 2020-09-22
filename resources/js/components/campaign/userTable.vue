a9633fa768ebdbb04c52650a44ae066ce615964b
python9066 <python9066@gmail.com>
2020-09-22 17:59:39 +0100

    quick commit

--- resources/js/components/campaign/userTable.vue
+++ resources/js/components/campaign/userTable.vue
@@ -1,59 +1,52 @@
 <template>
-<v-col cols="10">
-      <v-card
-      tile
-      >
-    <v-card-title  class="d-flex justify-space-between align-center ">
-        <div>Table of all Chars involed in this Campaign</div>
-        <div> <v-btn-toggle right-align v-model="toggle_exclusive" mandatory :value="0">
-                <v-btn
-                    @click="statusflag = 5"
+    <v-col cols="10">
+        <v-card tile>
+            <v-card-title class="d-flex justify-space-between align-center ">
+                <div>Table of all Chars involed in this Campaign</div>
+                <div>
+                    <v-btn-toggle
+                        right-align
+                        v-model="toggle_exclusive"
+                        mandatory
+                        :value="0"
+                    >
+                        <v-btn @click="statusflag = 5">
+                            All
+                        </v-btn>
+                        <v-btn @click="statusflag = 1">
+                            Hackers
+                        </v-btn>
+                        <v-btn @click="statusflag = 2">
+                            Scouts
+                        </v-btn>
+                        <v-btn @click="statusflag = 3">
+                            FCs
+                        </v-btn>
+                        <v-btn @click="statusflag = 4">
+                            Commands
+                        </v-btn>
+                    </v-btn-toggle>
+                </div>
+            </v-card-title>
+            <v-card-text>
+                <v-data-table
+                    :headers="headers"
+                    :items="filteredItems"
+                    item-key="id"
+                    disable-pagination
+                    fixed-header
+                    :height="tableHight()"
+                    hide-default-footer
+                    class="elevation-24"
+                    dense
                 >
-                    All
-                </v-btn>
-                <v-btn
-                    @click="statusflag = 1"
-                >
-                    Hackers
-                </v-btn>
-                <v-btn
-                    @click="statusflag = 2"
-                >
-                    Scouts
-                </v-btn>
-                <v-btn
-                    @click="statusflag = 3"
-                >
-                    FCs
-                </v-btn>
-                <v-btn
-                    @click="statusflag = 4"
-                >
-                    Commands
-                </v-btn>
-            </v-btn-toggle>
-        </div>
-    </v-card-title>
-    <v-card-text>
-        <v-data-table
-            :headers="headers"
-            :items="filteredItems"
-            item-key="id"
-            disable-pagination
-            fixed-header
-            height=150
-            hide-default-footer
-            class="elevation-24"
-            dense
-        >
-
-<template slot="no-data">
-                No one is here
-            </template>
-        </v-data-table>
-    </v-card-text>
-      </v-card>
-</v-col>
+                    <template slot="no-data">
+                        No one is here
+                    </template>
+                </v-data-table>
+            </v-card-text>
+        </v-card>
+    </v-col>
 </template>
 
 <script>
@@ -61,75 +54,136 @@ import { mapGetters } from "vuex";
 import { mapState } from "vuex";
 export default {
     props: {
-   campaign_id: String,
-},
+        campaign_id: String
+    },
     data() {
         return {
             headers: [
                 { text: "Name", value: "char_name", width: "10%" },
-                { text: "Main",value: "main_name"},
+                { text: "Main", value: "main_name" },
                 { text: "Ship", value: "ship" },
                 { text: "Entosic", value: "link" },
                 { text: "System", value: "system_name" },
                 { text: "Node", value: "node_id" },
                 { text: "Status", value: "user_status_name" },
-                { text: "Role", value: "role_name" },
+                { text: "Role", value: "role_name" }
 
                 // { text: "Vulernable End Time", value: "vulnerable_end_time" }
             ],
             statusflag: 0,
-            toggle_exclusive: 0,
-        }
+            toggle_exclusive: 0
+        };
     },
 
-
-
-
-
-
-
-
     computed: {
-
-        ...mapState([
-            "campaignusers",
-        ]),
+        ...mapState(["campaignusers"]),
 
         filteredItems() {
             // var timers = this.$store.state.timers;
             if (this.statusflag == 1) {
                 return this.campaignusers.filter(
-                    campaignusers => campaignusers.role_id == 1  && campaignusers.campaign_id == this.campaign_id
+                    campaignusers =>
+                        campaignusers.role_id == 1 &&
+                        campaignusers.campaign_id == this.campaign_id
                 );
             }
             if (this.statusflag == 2) {
                 return this.campaignusers.filter(
-                    campaignusers => campaignusers.role_id == 2  && campaignusers.campaign_id == this.campaign_id
+                    campaignusers =>
+                        campaignusers.role_id == 2 &&
+                        campaignusers.campaign_id == this.campaign_id
                 );
             }
             if (this.statusflag == 3) {
                 return this.campaignusers.filter(
-                    campaignusers => campaignusers.role_id == 3  && campaignusers.campaign_id == this.campaign_id
+                    campaignusers =>
+                        campaignusers.role_id == 3 &&
+                        campaignusers.campaign_id == this.campaign_id
                 );
             }
             if (this.statusflag == 4) {
                 return this.campaignusers.filter(
-                    campaignusers => campaignusers.role_id == 4  && campaignusers.campaign_id == this.campaign_id
+                    campaignusers =>
+                        campaignusers.role_id == 4 &&
+                        campaignusers.campaign_id == this.campaign_id
                 );
-            }
-            else {
+            } else {
                 return this.campaignusers.filter(
-                    campaignusers => campaignusers.role_id != 10  && campaignusers.campaign_id == this.campaign_id
+                    campaignusers =>
+                        campaignusers.role_id != 10 &&
+                        campaignusers.campaign_id == this.campaign_id
                 );
             }
         },
-
-
-    },
-
-}
+        tableHight() {
+            // var timers = this.$store.state.timers;
+            if (this.statusflag == 1) {
+                var count = this.campaignusers.filter(
+                    campaignusers =>
+                        campaignusers.role_id == 1 &&
+                        campaignusers.campaign_id == this.campaign_id
+                ).lenght;
+                count = count * 10;
+                if (count > 400) {
+                    return 400;
+                } else {
+                    return count;
+                }
+            }
+            if (this.statusflag == 2) {
+                var count = this.campaignusers.filter(
+                    campaignusers =>
+                        campaignusers.role_id == 2 &&
+                        campaignusers.campaign_id == this.campaign_id
+                ).lenght;
+                count = count * 10;
+                if (count > 400) {
+                    return 400;
+                } else {
+                    return count;
+                }
+            }
+            if (this.statusflag == 3) {
+                var count = this.campaignusers.filter(
+                    campaignusers =>
+                        campaignusers.role_id == 3 &&
+                        campaignusers.campaign_id == this.campaign_id
+                ).lenght;
+                count = count * 10;
+                if (count > 400) {
+                    return 400;
+                } else {
+                    return count;
+                }
+            }
+            if (this.statusflag == 4) {
+                var count = this.campaignusers.filter(
+                    campaignusers =>
+                        campaignusers.role_id == 4 &&
+                        campaignusers.campaign_id == this.campaign_id
+                ).lenght;
+                count = count * 10;
+                if (count > 400) {
+                    return 400;
+                } else {
+                    return count;
+                }
+            } else {
+                var count = this.campaignusers.filter(
+                    campaignusers =>
+                        campaignusers.role_id != 10 &&
+                        campaignusers.campaign_id == this.campaign_id
+                ).lenght;
+                count = count * 10;
+                if (count > 400) {
+                    return 400;
+                } else {
+                    return count;
+                }
+            }
+        }
+    }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
