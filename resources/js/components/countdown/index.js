f1f1d6bffc1a1fe67abba212b66991f289ca1d34
python9066 <python9066@gmail.com>
2020-09-22 01:36:04 +0100

    quick commit

--- resources/js/components/countdown/index.js
+++ resources/js/components/countdown/index.js
@@ -212,7 +212,8 @@ const CountDowntimer = {
             // Formating time - 格式化时间格式
             this.stop();
             this.$set(this, 'current', new Date().getTime())
-            const startCount =  new Date(this.formatTime(this.startTime)).getTime() - this.current ;
+            // const startCount =  new Date(this.formatTime(this.startTime)).getTime() - this.current ;
+            const startCount =  this.startTime - this.current ;
             console.log(startCount)
 
 
