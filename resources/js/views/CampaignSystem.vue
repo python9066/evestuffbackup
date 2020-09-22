583514a2c95d20acc21cc1e2c04787c403a617d4
python9066 <python9066@gmail.com>
2020-09-22 13:12:42 +0100

    quick commit

--- resources/js/views/CampaignSystem.vue
+++ resources/js/views/CampaignSystem.vue
@@ -365,7 +365,7 @@
                             </div></v-progress-circular
                         >
                     </div>
-                    <div class=" d-inline-flex justify-content-end align-content-center">
+                    <div class=" ml-auto">
                         <p> Finished Nodes - </p>
                         <v-progress-circular
                             class=" pr-3"
