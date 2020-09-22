af3267213cfda723bc3e155794bc89a578dc7f3b
python9066 <python9066@gmail.com>
2020-09-22 12:35:56 +0100

    quick commit

--- utils/Campaignhelper.php
+++ utils/Campaignhelper.php
@@ -150,8 +150,9 @@ class Campaignhelper
             ->count();
 
         $b_node_new = $campaign->b_node + $b_node_add;
+        $r_node_new = $campaign->r_node + $r_node_add;
 
-        echo " ---".$b_node_new.  "----  ";
+        $campaign->update(["b_node" =>$b_node_new, "r_node" =>$r_node_new ]);
 
         $campaign->campaignsystems()
             ->where('campaign_system_status_id', 4)
