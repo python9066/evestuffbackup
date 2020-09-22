c471ccdb3d3a517ef3a82ec5fb0b66b0722cd618
python9066 <python9066@gmail.com>
2020-09-22 13:18:44 +0100

    quick commit

--- resources/js/views/CampaignSystem.vue
+++ resources/js/views/CampaignSystem.vue
@@ -366,7 +366,7 @@
                         >
                     </div>
                     <v-spacer></v-spacer>
-                    <div v-if="campaign.total_node > 0" class=" ml-auto d-inline-flex align-content-center">
+                    <div v-if="campaign.total_node > 0" class=" ml-auto d-inline-flex align-center">
                         <p> Finished Nodes - </p>
                         <v-progress-circular
                             class=" pr-3"
