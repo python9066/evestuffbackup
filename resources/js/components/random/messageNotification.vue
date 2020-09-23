f412c91b0893e407eafe03de023fb0c71999271d
python9066 <python9066@gmail.com>
2020-09-23 22:55:58 +0100

    quick commit

---
+++ resources/js/components/random/messageNotification.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-alert
+      outlined
+      dismissible
+      text
+      type="warning"
+      :icon = false
+      :elevation = 24
+    >
+      <p>This page has been updated so you now <strong>need roles</strong> to edit the noficiations.  Contact a<strong> Recon or Coord Memeber</strong> for roles.</p>
+      <p>If you find any problems, contact <strong>JohnMonty</strong> on Jabber  Doing some testing, so will see old notifications</p>
+    </v-alert>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "messageComponent",
+};
+</script>
+
+
+
