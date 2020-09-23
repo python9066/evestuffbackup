d322f50ca36b817978a522f7d18df56178163d0b
python9066 <python9066@gmail.com>
2020-09-23 19:54:52 +0100

    quick commit

--- resources/js/views/AdminPanel.vue
+++ resources/js/views/AdminPanel.vue
@@ -88,7 +88,7 @@
                         :footer-props="{
                             'items-per-page-options': [15, 25, 50, 100, -1]
                         }"
-                        :sort-by="['start']"
+                        :sort-by="['name']"
                         :sort-desc="[false, true]"
                         multi-sort
                         class="elevation-1"
@@ -140,6 +140,7 @@
                                 >
                                     <v-chip
                                         pill
+                                        class="rainbow-button"
                                         :close="pillClose(role.name)"
                                         dark
                                         @click:close="
@@ -315,3 +316,48 @@ export default {
     beforeDestroy() {}
 };
 </script>
+<style>
+body {
+  margin:0;
+  background-color:#191919;
+  min-height:100vh;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  font-family:Helvetica,Sans-serif;
+}
+a {
+  text-decoration:none;
+  color:#FFF;
+}
+.rainbow-button {
+  width:calc(20vw + 6px);
+  height:calc(8vw + 6px);
+  background-image: linear-gradient(90deg, #00C0FF 0%, #FFCF00 49%, #FC4F4F 80%, #00C0FF 100%);
+  border-radius:5px;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  text-transform:uppercase;
+  font-size:3vw;
+  font-weight:bold;
+}
+.rainbow-button:after {
+  content:attr(alt);
+  width:20vw;
+  height:8vw;
+  background-color:#191919;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+}
+.rainbow-button:hover {
+  animation:slidebg 2s linear infinite;
+}
+
+@keyframes slidebg {
+  to {
+    background-position:20vw;
+  }
+}
+</style>
