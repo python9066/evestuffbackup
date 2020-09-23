9fd2db44c3cc0bbd915ef796f8d99c1a820b3ca3
python9066 <python9066@gmail.com>
2020-09-23 22:59:24 +0100

    quick commit

--- resources/js/components/random/messageNotification.vue
+++ resources/js/components/random/messageNotification.vue
@@ -9,7 +9,7 @@
       :elevation = 24
     >
       <p>This page has been updated so you now <strong>need roles</strong> to edit the noficiations.  Contact a<strong> Recon or Coord Memeber</strong> for roles.</p>
-      <p>If you find any problems, contact <strong>JohnMonty</strong> on Jabber  Doing some testing, so will see old notifications</p>
+      <p>If you find any problems, contact <strong>JohnMonty</strong> on Jabber</p>
     </v-alert>
   </div>
 </template>
