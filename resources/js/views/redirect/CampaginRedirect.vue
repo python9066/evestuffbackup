9cd6fca32170ed763859970a3224a0f5b746febc
python9066 <python9066@gmail.com>
2020-09-24 11:59:16 +0100

    quick commit

--- resources/js/views/redirect/CampaginRedirect.vue
+++ resources/js/views/redirect/CampaginRedirect.vue
@@ -5,7 +5,7 @@
     <v-card>
         <v-card-title class=" justify-center"><h1>Why am I on this page?</h1></v-card-title>
         <v-card-text>
-            <span class=" body-1">
+            <span class=" body-1 justify-center">
         <p>You do not have the required roles to access this page.  If in a fleet let your FC know and they should be able to give you roles</p>
         <p>If the FC can't give you roles have them contact a member of Recon, Coord or GSFOE.</p>
             </span>
