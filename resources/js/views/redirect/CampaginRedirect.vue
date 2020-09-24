a190351b4d408e483b10be0cdb497d55b5fe9bfd
python9066 <python9066@gmail.com>
2020-09-24 11:32:17 +0100

    quick commit

--- resources/js/views/redirect/CampaginRedirect.vue
+++ resources/js/views/redirect/CampaginRedirect.vue
@@ -1,7 +1,7 @@
 <template>
     <div class=" pr-16 pl-16">
 
-<div class=" d-inline" cols="8">
+<div class=" d-inline align-content-center" cols="8">
     <v-col cols="8">
     <v-card>
         <v-card-title raised><h1> Why am I on this page? </h1></v-card-title>
