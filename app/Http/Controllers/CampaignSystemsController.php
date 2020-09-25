d1f61d98e5627c3a67a6873d636679284868f785
python9066 <python9066@gmail.com>
2020-09-25 23:51:42 +0100

    quick commit

--- app/Http/Controllers/CampaignSystemsController.php
+++ app/Http/Controllers/CampaignSystemsController.php
@@ -76,6 +76,7 @@ class CampaignSystemsController extends Controller
             $node->update(['campaign_user_id' =>  null]);
             $test = CampaignSystem::where('campaign_id',$request->campaign_id)
             ->where('system_id',$request->system_id)->get();
+            echo "yo";
 
 
 
