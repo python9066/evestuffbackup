16d8a050d2debc3c2046de803860e67f2776ba20
python9066 <python9066@gmail.com>
2020-09-25 13:30:29 +0100

    quick commit

--- resources/js/components/campaign/systemTable.vue
+++ resources/js/components/campaign/systemTable.vue
@@ -304,7 +304,7 @@
                         </VueCountUptimer>
                         <v-menu
                             :close-on-content-click="false"
-                            :v-value="timerShown[item.id]"
+                            :v-model="timerShown[item.id]"
                             v-else-if="checkHackUser(item)"
                         >
                             <template v-slot:activator="{ on, attrs,}">
