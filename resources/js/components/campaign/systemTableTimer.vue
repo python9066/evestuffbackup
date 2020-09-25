e53c37756f03402f3a892f09fc2d2665372c755a
python9066 <python9066@gmail.com>
2020-09-25 13:55:24 +0100

    quick commit

---
+++ resources/js/components/campaign/systemTableTimer.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-col>
+    </v-col>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+import moment from "moment";
+export default {
+    props: {
+        system_name: String,
+        system_id: Number,
+        campaign_id: String
+    },
+    data() {
+        return {
+        };
+    },
+
+    methods: {
+    },
+
+    computed: {
+    }
+};
+</script>
+
+<style>
+
+.style-2 {
+  background-color: rgb(30,30,30,1)
+}
+.style-1 {
+  background-color: rgb(184, 22, 35)
+}</style>
