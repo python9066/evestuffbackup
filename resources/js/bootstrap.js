3935d1592e1b4fd8c02e47ae31bda62ea0949818
python9066 <python9066@gmail.com>
2020-09-26 17:58:13 +0100

    quick commit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -36,6 +36,6 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    useTLS: true
+    useTLS: false
 
 });
