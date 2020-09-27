737b4c0eb44896aec65c428e6d2397aa682bfbae
python9066 <python9066@gmail.com>
2020-09-27 14:42:36 +0100

    quick commit

--- resources/js/components/campaign/systemTable.vue
+++ resources/js/components/campaign/systemTable.vue
@@ -348,7 +348,7 @@
                             <div>
                                 <v-col class="align-center">
                                     <v-textarea
-                                        v-bind:value="noteText"
+                                        v-bind:value="item.notes"
                                         label="Where you can put any notes"
                                         outlined
                                         shaped
