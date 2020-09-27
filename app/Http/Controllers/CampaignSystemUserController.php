87fcecad37f9505d716ac2c1b4ffbcf30ebed804
python9066 <python9066@gmail.com>
2020-09-27 10:44:01 +0100

    quick commit

--- app/Http/Controllers/CampaignSystemUserController.php
+++ app/Http/Controllers/CampaignSystemUserController.php
@@ -16,7 +16,7 @@ class CampaignSystemUserController extends Controller
      */
     public function index($campid)
     {
-        $test = CampaignSystemUsers::where('campaign_id',$campid);
+        $test = CampaignSystemUsers::where('campaign_id',$campid)->user()->get();
         $test2 =$test;
         dd($test,$test2);
         return [ 'users' => CampaignSystemUsers::with('user')->where('campaign_id',$campid)->get()];
