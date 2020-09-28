ace706c4e9ca378abfc4f57a686636f9e5e3a3be
python9066 <python9066@gmail.com>
2020-09-29 00:42:27 +0100

    quick commit

--- resources/js/store.js
+++ resources/js/store.js
@@ -141,6 +141,7 @@ export default new Vuex.Store({
 
 
         async getUsers({ commit, state }) {
+            console.log(state.token)
             let res = await axios({
                 method: "get",
                 url: "/api/allusersroles",
