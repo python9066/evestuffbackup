59ac635b646d24f145007672f7bd013be485831f
python9066 <python9066@gmail.com>
2020-09-30 18:12:11 +0100

    quick commit

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::get('/oauth/callback', 'AuthController@handleProviderCallback');
 Route::get('/logout', 'AuthController@logout');
 Route::get('/home', 'HomeController@index');
 // Route::get('/pannel/{any}', 'RoleController@addCord')->where('any', '.*');
-// Route::get('/updateNotifications', 'NotificationController@getNotifications');
+Route::get('/updateNotifications', 'NotificationController@getNotifications');
 // Route::get('/test2', 'UserRolesRecordsController@index');
 // Route::get('/updateAlliances', 'AllianceController@updateAlliances');
 // Route::get('/party2', 'HomeController@party2');
