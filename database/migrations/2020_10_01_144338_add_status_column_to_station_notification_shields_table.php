08e707fcfea87bc09f0c513bad08f86a564fc4ba
python9066 <python9066@gmail.com>
2020-10-01 15:48:06 +0100

    quick commit

--- database/migrations/2020_10_01_144338_add_status_column_to_station_notification_shields_table.php
+++ database/migrations/2020_10_01_144338_add_status_column_to_station_notification_shields_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddStatusColumnToStationNotificationSheildsTable extends Migration
+class AddStatusColumnToStationNotificationShieldsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AddStatusColumnToStationNotificationSheildsTable extends Migration
      */
     public function up()
     {
-        Schema::table('station_notification_sheilds', function (Blueprint $table) {
+        Schema::table('station_notification_shields', function (Blueprint $table) {
             $table->integer('status')->after('timestamp')->default(0);
         });
     }
@@ -25,7 +25,7 @@ class AddStatusColumnToStationNotificationSheildsTable extends Migration
      */
     public function down()
     {
-        Schema::table('station_notification_sheilds', function (Blueprint $table) {
+        Schema::table('station_notification_shields', function (Blueprint $table) {
             //
         });
     }
