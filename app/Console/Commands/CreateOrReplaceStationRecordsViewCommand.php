f2725b1a9133f6014efdda2f9b5de4fb4837d68d
python9066 <python9066@gmail.com>
2020-10-01 21:34:50 +0100

    quick commit

---
+++ app/Console/Commands/CreateOrReplaceStationRecordsViewCommand.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class CreateOrReplaceStationRecordsViewCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'view:CreateOrReplaceStationRecordsView';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Will make the view table for Stations';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+       DB::statement("CREATE VIEW station_records SELECT stations.id AS 'id',
+       stations.name AS 'station_name',
+       stations.station_status_id AS 'system_id',
+       systems.system_name AS 'system_name',
+       constellations.constellation_name AS 'constellation_name',
+       regions.region_name AS 'region_name',
+       stations.item_id AS 'item_id',
+       items.item_name AS 'item_name',
+       stations.user_id AS 'user_id',
+       users.name AS 'user_name',
+       stations.station_status_id AS 'station_status_id',
+       station_statuses.name AS 'station_status_name',
+       stations.timestamp AS 'timestamp',
+       stations.text AS 'text'
+       FROM stations
+       JOIN systems ON systems.id = stations.system_id
+       JOIN constellations ON constellations.id = systems.constellation_id
+       JOIN regions ON regions.id = systems.region_id
+       JOIN items ON items.id = stations.item_id
+       JOIN station_statuses ON station_statuses.id = stations.station_status_id
+       LEFT JOIN users on users.id = stations.user_id
+       WHERE stations.station_status_id != 10");
+    }
+}
---
+++ database/migrations/2020_10_01_223300_call_the_campaign_records_command copy.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Artisan;
+
+class CallTheStationRecordsCommand extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Artisan::call("view:CreateOrReplaceCampaignSystemRecordsView");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
