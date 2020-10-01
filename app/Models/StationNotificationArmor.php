93d38f81253bd222af378dcdcfab4083d3798d6a
python9066 <python9066@gmail.com>
2020-10-01 15:28:12 +0100

    quick commit

--- app/Models/StationNotificationArmor.php
+++ app/Models/StationNotificationArmor.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class StationNotificationArmor extends Model
 {
     use HasFactory;
+    protected $guarded =[];
 }
--- app/Models/StationStatus.php
+++ app/Models/StationStatus.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class StationNotificationShield extends Model
+class StationStatus extends Model
 {
     use HasFactory;
     protected $guarded =[];
