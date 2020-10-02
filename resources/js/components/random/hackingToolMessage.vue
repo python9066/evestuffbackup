07365327bdbaacb88278fd9ffc4a627bc6dadc86
python9066 <python9066@gmail.com>
2020-10-02 22:06:27 +0100

    quick commit

--- resources/js/components/random/hackingToolMessage.vue
+++ resources/js/components/random/hackingToolMessage.vue
@@ -8,8 +8,8 @@
       :icon = false
       :elevation = 24
     >
-     <p class="text-center"> This whole page is very work in progress so things may not work fully.  Contact <strong> JohnMonty</strong> on Jabber you see anything 100% broken to the point its blowing up.</p>
-     <p class="text-center"> If you want play around feel free.  Take note. To assign yourself to a node, you <strong>HAVE TO</strong> press the "ADD CHAR" button and pick<strong> Hacker </strong>as your role.</p>
+     <p class="text-center"> This page should now be working as intended.  Contact <strong> JohnMonty</strong> on Jabber you see anything broken.</p>
+     <p class="text-center"> Take note. To assign yourself to a node, you <strong>HAVE TO</strong> press the "ADD CHAR" button and pick<strong> Hacker </strong>as your role.</p>
     </v-alert>
   </div>
 </template>
