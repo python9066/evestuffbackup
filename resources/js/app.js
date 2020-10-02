2af649043aa858cfdbd32fa6ca2fb70b86a891b3
python9066 <python9066@gmail.com>
2020-10-02 12:09:58 +0100

    quick commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,7 +14,7 @@ import App from "./views/App";
 import colors from "vuetify/lib/util/colors";
 import VueCountdown from '@chenfengyuan/vue-countdown';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import moment from 'moment'
 import VueCountdownTimer from 'vuejs-countdown-timer'
@@ -22,6 +22,8 @@ import VueCountupTimer from './components/countup/index'
 import CountdownTimer from './components/countdown/index'
 import VueMask from 'v-mask'
 import Permissions from './mixins/Permissions.vue'
+
+library.add(faUserSecret)
 
 Vue.component('messageComponent', require('./components/random/messageComponent.vue').default);
 Vue.component('errorMessage', require('./components/random/errorMessage.vue').default); //component name should be in camel-case
@@ -39,9 +41,7 @@ Vue.prototype.moment = moment
 Vue.use(CountdownTimer)
 Vue.use(VueCountdownTimer)
 Vue.use(VueCountupTimer)
-// library.add(faUserSecret)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-library.add(fas) // Include needed icons
 Vue.config.productionTip = false
 Vue.component(VueCountdown.name, VueCountdown);
 Vue.use(VueMask)
@@ -76,9 +76,6 @@ export default new Vuetify({
             "dark-orange": "#F57C00"
         },
       dark: true,
-    },
-    icons: {
-        iconfont: 'faSvg',
     },
   })
 
