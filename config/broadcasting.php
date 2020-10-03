aad671f782973a4951f079f749bf6b7ed20d431e
python9066 <python9066@gmail.com>
2020-10-03 18:12:56 +0100

    quick commit

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -30,19 +30,6 @@ return [
 
     'connections' => [
 
-        'pusher' => [
-            'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
-            'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
-                'disableStats' =>true
-
-            ],
-        ],
-
         // 'pusher' => [
         //     'driver' => 'pusher',
         //     'key' => env('PUSHER_APP_KEY'),
@@ -51,20 +38,24 @@ return [
         //     'options' => [
         //         'cluster' => env('PUSHER_APP_CLUSTER'),
         //         'useTLS' => true,
-        //         'useTLS' => false,
-        //         'encrupted' => false,
-        //         'disableStats' =>true,
-        //         'host' => 'evewebsockets.club',
-        //         'port' => '6001',
-        //         'scheme' => 'https',
-        //         'scheme' => 'http',
-        //         'curl_options' => [
-        //             CURLOPT_SSL_VERIFYHOST => 0,
-        //             CURLOPT_SSL_VERIFYPEER => 0,
-        //         ]
+        //         'disableStats' =>true
 
         //     ],
         // ],
+
+        'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'encrypted' => true,
+                'host' => 'evewebsockets.club',
+                'port' => 6001,
+                'scheme' => 'https'
+            ],
+        ],
 
         'redis' => [
             'driver' => 'redis',
