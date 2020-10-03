c8bb2736a36c647b40fc6b6aa026ec00f07ca9f1
python9066 <python9066@gmail.com>
2020-10-03 20:16:20 +0100

    quick commit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -32,23 +32,23 @@ import Echo from 'laravel-echo';
 window.Pusher = require('pusher-js');
 
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     useTLS: true,
-//     disableStats: true,
-
-// });
-
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    wsHost: 'evewebsockets.club',
-    forceTLS: false,
+    useTLS: true,
     disableStats: true,
-    wsPort: 6001,
-    wssPort: 443,
-    enabledTransports: ['ws', 'wss'],
+
 });
+
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     wsHost: 'evewebsockets.club',
+//     forceTLS: false,
+//     disableStats: true,
+//     wsPort: 6001,
+//     wssPort: 443,
+//     enabledTransports: ['ws', 'wss'],
+// });
