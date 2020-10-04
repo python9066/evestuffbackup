ae0d18908ebf05a5d1e9fa26bb0d04c3eceb7a04
python9066 <python9066@gmail.com>
2020-10-04 11:44:31 +0100

    quick commit

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -51,9 +51,10 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
+                'forceTLS' => true,
                 'host' => 'https://socket.evestuff.online',
                 'port' => 6001,
-                'scheme' => 'https',
+                'scheme' => 'http',
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
