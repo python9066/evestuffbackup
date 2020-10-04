687d6d677e5270866d6d95d1b55c1c1d32acf231
python9066 <python9066@gmail.com>
2020-10-04 11:45:14 +0100

    quick commit

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -30,19 +30,6 @@ return [
 
     'connections' => [
 
-        // 'pusher' => [
-        //     'driver' => 'pusher',
-        //     'key' => env('PUSHER_APP_KEY'),
-        //     'secret' => env('PUSHER_APP_SECRET'),
-        //     'app_id' => env('PUSHER_APP_ID'),
-        //     'options' => [
-        //         'cluster' => env('PUSHER_APP_CLUSTER'),
-        //         'useTLS' => true,
-        //         'disableStats' =>true
-
-        //     ],
-        // ],
-
         'pusher' => [
             'driver' => 'pusher',
             'key' => env('PUSHER_APP_KEY'),
@@ -50,17 +37,30 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
-                'forceTLS' => true,
-                'host' => 'https://socket.evestuff.online',
-                'port' => 6001,
-                'scheme' => 'http',
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ]
+                'useTLS' => true,
+                'disableStats' =>true
+
             ],
         ],
+
+        // 'pusher' => [
+        //     'driver' => 'pusher',
+        //     'key' => env('PUSHER_APP_KEY'),
+        //     'secret' => env('PUSHER_APP_SECRET'),
+        //     'app_id' => env('PUSHER_APP_ID'),
+        //     'options' => [
+        //         'cluster' => env('PUSHER_APP_CLUSTER'),
+        //         'encrypted' => true,
+        //         'forceTLS' => true,
+        //         'host' => 'https://socket.evestuff.online',
+        //         'port' => 6001,
+        //         'scheme' => 'http',
+        //         'curl_options' => [
+        //             CURLOPT_SSL_VERIFYHOST => 0,
+        //             CURLOPT_SSL_VERIFYPEER => 0,
+        //         ]
+        //     ],
+        // ],
 
 
         'redis' => [
