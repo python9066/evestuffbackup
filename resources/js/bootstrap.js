121fa0ce02a4b06d12458ae21716f212d87befdf
python9066 <python9066@gmail.com>
2020-10-05 11:53:07 +0100

    quick commit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -32,14 +32,14 @@ import Echo from 'laravel-echo';
 window.Pusher = require('pusher-js');
 
 
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    useTLS: true,
-    disableStats: true,
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     useTLS: true,
+//     disableStats: true,
 
-});
+// });
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
