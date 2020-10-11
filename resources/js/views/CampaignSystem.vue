3ea1b310264cee380cb7e713c7cdf41bf13f0aad
python9066 <python9066@gmail.com>
2020-10-11 14:54:29 +0100

    quick commit

--- resources/js/views/CampaignSystem.vue
+++ resources/js/views/CampaignSystem.vue
@@ -596,7 +596,7 @@ export default {
 
                 if (e.flag.flag == 6) {
                      console.log(6);
-                    this.kickUser(e.user_id);
+                    this.kickUser(e.flag.user_id);
                 }
             },
 
@@ -634,6 +634,7 @@ export default {
         },
 
         kickUser(user_id){
+            console.log(user_id + "---")
             if(this.$store.state.user_id == user_id){
                 console.log("kick")
             }
