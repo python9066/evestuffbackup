c80487bfe5762c89d20a0913417434019ea84da0
python9066 <python9066@gmail.com>
2020-10-23 22:55:55 +0100

    quick commit

---
+++ database/migrations/2020_10_23_214042_create_custom_campaigns_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomCampaignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('custom_campaigns', function (Blueprint $table) {
+            $table->id();
+            $table->integer('status_id')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('custom_campaigns');
+    }
+}
