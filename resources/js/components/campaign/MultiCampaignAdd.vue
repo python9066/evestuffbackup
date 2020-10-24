6e3594af994a851fe5a4eb08e39099d5c50d4494
python9066 <python9066@gmail.com>
2020-10-24 20:44:43 +0100

    quick commit

---
+++ resources/js/components/campaign/MultiCampaignAdd.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+import moment from "moment";
+export default {
+    data() {
+        return {
+
+        };
+    },
+
+    methods: {
+
+
+
+
+    },
+
+    computed: {
+
+    }
+};
+</script>
