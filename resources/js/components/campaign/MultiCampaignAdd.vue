3a52aa1f8146530597be27927d519d5fee396789
python9066 <python9066@gmail.com>
2020-10-24 21:01:53 +0100

    quick commit

--- resources/js/components/campaign/MultiCampaignAdd.vue
+++ resources/js/components/campaign/MultiCampaignAdd.vue
@@ -41,14 +41,7 @@ export default {
 
 
     created() {
-        Echo.private("multicampaigns").listen("CampaignChanged", e => {
-            this.loadcampaigns();
-        }),
-            this.$store.dispatch("getCampaigns").then(() => {
-                this.loadingf = false;
-                this.loadingr = false;
-                this.loading = false;
-            });
+            this.$store.dispatch("getCampaigns")
     },
 
     methods: {
