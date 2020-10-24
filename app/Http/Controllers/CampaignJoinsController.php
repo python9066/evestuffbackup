1d7912587af8679122a33f8d5362df401abc78d0
python9066 <python9066@gmail.com>
2020-10-25 00:29:14 +0100

    quick commit

--- app/Http/Controllers/CampaignJoinsController.php
+++ app/Http/Controllers/CampaignJoinsController.php
@@ -36,7 +36,7 @@ class CampaignJoinsController extends Controller
      */
     public function show($id)
     {
-        $pull = CampaignJoin::where('custom_campaign_id',$id)->with("campaignrecords")->get();
+        $pull = CampaignJoin::where('custom_campaign_id',$id)->campaignrecords->get();
         foreach ($pull as $pull){
             dd($pull);
         }
