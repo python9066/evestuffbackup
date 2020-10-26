bda921753f8fd184cf7985fc1df129d3af25fdfc
python9066 <python9066@gmail.com>
2020-10-26 13:25:12 +0000

    quick commit

--- resources/js/components/multicampaigns/TitleBar.vue
+++ resources/js/components/multicampaigns/TitleBar.vue
@@ -2,13 +2,17 @@
     <v-row no-gutters v-if="this.getCampaignsCount > 1" justify="space-around">
         <v-col md="10">
             <v-card class="pa-2" tile width="100%">
-                <v-card-title align="end" class="justify-center">
-                    <caption>
+                <v-card-title align="center" class="justify-center">
+                    <p>
                         Campaign page for the
                         {{ this.campaign.item_name }} in
                         {{ this.campaign.system }} -
+                        <v-avatar size="35"
+                            ><img :src="this.campaign.url"
+                        /></v-avatar>
+                        -
                         {{ this.campaign.alliance }}
-                    </caption>
+                    </p>
                 </v-card-title>
                 <div
                     class="d-flex full-width align-content-center"
