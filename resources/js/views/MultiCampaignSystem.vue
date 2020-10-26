366aa11f056b15e663e7c43b2034a9f6d33a292b
python9066 <python9066@gmail.com>
2020-10-26 10:34:57 +0000

    quick commit

--- resources/js/views/MultiCampaignSystem.vue
+++ resources/js/views/MultiCampaignSystem.vue
@@ -387,42 +387,42 @@ export default {
     },
 
     async created() {
-        Echo.private("campaignsystem." + this.$route.params.id).listen(
-            "CampaignSystemUpdate",
-            e => {
-                // console.log(e);
-                if (e.flag.flag == 1) {
-                    // console.log(1);
-                    this.loadUsersRecords();
-                }
-                if (e.flag.flag == 2) {
-                    // console.log(2);
-                    this.loadCampaignSystemRecords();
-                }
-                if (e.flag.flag == 3) {
-                    // console.log(3);
-                    this.loadCampaignSystemRecords();
-                    this.loadUsersRecords();
-                }
-                if (e.flag.flag == 4) {
-                    // console.log(4);
-                    this.loadcampaigns();
-                    this.loadCampaignSystemRecords();
-                    this.loadUsersRecords();
-                }
-                if (e.flag.flag == 5) {
-                    // console.log(4);
-                    this.checkAddUser();
-                }
+        // Echo.private("campaignsystem." + this.$route.params.id).listen(
+        //     "CampaignSystemUpdate",
+        //     e => {
+        //         // console.log(e);
+        //         if (e.flag.flag == 1) {
+        //             // console.log(1);
+        //             this.loadUsersRecords();
+        //         }
+        //         if (e.flag.flag == 2) {
+        //             // console.log(2);
+        //             this.loadCampaignSystemRecords();
+        //         }
+        //         if (e.flag.flag == 3) {
+        //             // console.log(3);
+        //             this.loadCampaignSystemRecords();
+        //             this.loadUsersRecords();
+        //         }
+        //         if (e.flag.flag == 4) {
+        //             // console.log(4);
+        //             this.loadcampaigns();
+        //             this.loadCampaignSystemRecords();
+        //             this.loadUsersRecords();
+        //         }
+        //         if (e.flag.flag == 5) {
+        //             // console.log(4);
+        //             this.checkAddUser();
+        //         }
 
-                if (e.flag.flag == 6) {
-                    //  console.log(6);
-                    this.kickUser(e.flag.user_id);
-                }
-            },
+        //         if (e.flag.flag == 6) {
+        //             //  console.log(6);
+        //             this.kickUser(e.flag.user_id);
+        //         }
+        //     },
 
-            window.addEventListener("beforeunload", this.leaving)
-        );
+        //     window.addEventListener("beforeunload", this.leaving)
+        // );
         this.channel = "campaignsystem." + this.$route.params.id;
         this.campaignId = this.$route.params.id;
         this.test = 2;
