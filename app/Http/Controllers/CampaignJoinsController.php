260f371d27bc81aaffa98b90342cb58270b56caf
python9066 <python9066@gmail.com>
2020-10-26 10:58:57 +0000

    quick commit

--- app/Http/Controllers/CampaignJoinsController.php
+++ app/Http/Controllers/CampaignJoinsController.php
@@ -57,7 +57,7 @@ class CampaignJoinsController extends Controller
                 $data = [];
                 $data = [
                     "id" => $camp['system_id'],
-                    "system_name" => ['system_name']
+                    "system_name" => $camp['system_name']
                 ];
             }
             array_push($list, $data);
