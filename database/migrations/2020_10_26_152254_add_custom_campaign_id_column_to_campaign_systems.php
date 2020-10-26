d2c3ca49bd5a3a50eef5a387f086734d157d5932
python9066 <python9066@gmail.com>
2020-10-26 15:26:07 +0000

    quick commit

--- database/migrations/2020_10_26_152254_add_custom_campaign_id_column_to_campaign_systems.php
+++ database/migrations/2020_10_26_152254_add_custom_campaign_id_column_to_campaign_systems.php
@@ -14,7 +14,7 @@ class AddCustomCampaignIdColumnToCampaignSystems extends Migration
     public function up()
     {
         Schema::table('campaign_systems', function (Blueprint $table) {
-            //
+            $table->foreignId('custom_campaign_id')->after('campaign_id')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddCustomCampaignIdColumnToCampaignSystems extends Migration
     public function down()
     {
         Schema::table('campaign_systems', function (Blueprint $table) {
-            $table->foreignId('custom_campaign_id')->after('campaign_id')->nullable();
+
         });
     }
 }
