641eb7f724d50e0438b962c09bacd1c7cfb1ac00
python9066 <python9066@gmail.com>
2020-10-26 12:43:21 +0000

    quick commit

--- resources/js/components/multicampaigns/TitleBar.vue
+++ resources/js/components/multicampaigns/TitleBar.vue
@@ -200,61 +200,13 @@ export default {
     methods: {
         barColor() {
             var d =
-                this.getCampaignById(this.sCampaignID).defenders_score *
+                this.getCampaignById(this.campaignId).defenders_score *
                 100;
             if (d > 50) {
                 return "blue darken-4";
             }
 
             return "red darken-4";
-        },
-
-        barScoure() {
-            var d =
-                this.getCampaignById(this.sCampaignID).defenders_score *
-                100;
-            var a =
-                this.getCampaignById(this.sCampaignID).attackers_score *
-                100;
-
-            if (d > 50) {
-                return d;
-            }
-
-            return a;
-        },
-
-        barActive() {
-            if (this.getCampaignById(this.sCampaignID).status_id > 1) {
-                return true;
-            }
-            return false;
-        },
-
-        barBgcolor() {
-            var d =
-                this.getCampaignById(this.sCampaignID).defenders_score *
-                100;
-            var a =
-                this.getCampaignById(this.sCampaignID).attackers_score *
-                100;
-
-            if (d > 50) {
-                return "red darken-4";
-            }
-
-            return "blue darken-4";
-        },
-
-        barReverse() {
-            var d =
-                this.getCampaignById(this.sCampaignID).defenders_score *
-                100;
-            if (d > 50) {
-                return false;
-            }
-
-            return true;
         },
     },
 
