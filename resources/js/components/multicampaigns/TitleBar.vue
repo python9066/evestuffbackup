822947ce7a25ead2a6303421fbf0045ab50d3686
python9066 <python9066@gmail.com>
2020-10-26 10:32:13 +0000

    quick commit

--- resources/js/components/multicampaigns/TitleBar.vue
+++ resources/js/components/multicampaigns/TitleBar.vue
@@ -1,38 +1,184 @@
 <template>
-    <div>
-        <v-card min-width=1200 max-width=1200>
-            <v-card-title>
-                Make your Mulit-Campaign Here
-            </v-card-title>
-            <v-card-text>
-                <v-text-field
-                    label="Multi-Campaign Name"
-                    v-model="name"
-                    hint="Enter The name of your Campaign here"
-                    filled
+    <v-row
+        no-gutters
+        v-if="this.getCampaignsCount > 1"
+        class="pt-5"
+        justify="space-around"
+    >
+        <v-col md="10">
+            <v-card class="pa-2" tile width="100%">
+                <v-card-title align="center" class="justify-center">
+                    <h1>
+                        Campaign page for the
+                        {{ this.campaign.item_name }} in
+                        {{ this.campaign.system }} -
+                        <v-avatar size="35"
+                            ><img :src="this.campaign.url"
+                        /></v-avatar>
+                        -
+                        {{ this.campaign.alliance }}
+                    </h1>
+                </v-card-title>
+                <div
+                    class="d-flex full-width align-content-center"
+                    v-if="this.campaign.status_id > 1"
                 >
-                </v-text-field>
-                <v-select
-                    v-model="picked"
-                    :items="list"
-                    label="Select"
-                    multiple
-                    chips
-                    deletable-chips
-                    hint="Which Campaigns do you want"
-                    persistent-hint
-                ></v-select>
-            </v-card-text>
-            <v-card-actions>
-                <v-btn color="success" class="mr-4" @click="addCampaignDone()"
-                    >Done</v-btn
+                    <v-icon
+                        v-if="
+                            this.campaign.defenders_score >
+                                this.campaign.defenders_score_old &&
+                                this.campaign.defenders_score_old > 0
+                        "
+                        small
+                        left
+                        dark
+                        color="blue darken-4"
+                    >
+                        fas fa-arrow-alt-circle-up
+                    </v-icon>
+                    <v-icon
+                        v-if="
+                            this.campaign.defenders_score <
+                                this.campaign.defenders_score_old &&
+                                this.campaign.defenders_score_old > 0
+                        "
+                        small
+                        left
+                        dark
+                        color="blue darken-4"
+                    >
+                        fas fa-arrow-alt-circle-down
+                    </v-icon>
+                    <v-icon
+                        v-if="
+                            this.campaign.defenders_score ==
+                                this.campaign.defenders_score_old ||
+                                this.campaign.defenders_score_old === null
+                        "
+                        small
+                        left
+                        dark
+                        color="grey darken-3"
+                    >
+                        fas fa-minus-circle
+                    </v-icon>
+
+                    <v-progress-linear
+                        :color="this.barColor"
+                        :value="this.barScoure"
+                        height="20"
+                        rounded
+                        :active="this.barActive"
+                        :reverse="this.barReverse"
+                        :background-color="this.barBgcolor"
+                        background-opacity="0.2"
+                    >
+                        <strong>
+                            {{ this.campaign.defenders_score * 100 }} /
+                            {{ this.campaign.attackers_score * 100 }}
+                        </strong>
+                    </v-progress-linear>
+
+                    <v-icon
+                        v-if="
+                            this.campaign.attackers_score >
+                                this.campaign.attackers_score_old &&
+                                this.campaign.attackers_score_old > 0
+                        "
+                        small
+                        right
+                        dark
+                        color="red darken-4"
+                    >
+                        fas fa-arrow-alt-circle-up
+                    </v-icon>
+                    <v-icon
+                        v-if="
+                            this.campaign.attackers_score <
+                                this.campaign.attackers_score_old &&
+                                this.campaign.attackers_score_old > 0
+                        "
+                        small
+                        right
+                        dark
+                        color="red darken-4"
+                    >
+                        fas fa-arrow-alt-circle-down
+                    </v-icon>
+                    <v-icon
+                        v-if="
+                            this.campaign.attackers_score ==
+                                this.campaign.attackers_score_old ||
+                                this.campaign.attackers_score_old == null
+                        "
+                        small
+                        right
+                        dark
+                        color="grey darken-3"
+                    >
+                        fas fa-minus-circle
+                    </v-icon>
+                </div>
+                <div
+                    class="d-flex full-width align-content-center"
+                    v-if="this.campaign.status_id == 1"
                 >
-                <v-btn color="warning" class="mr-4" @click="addCampaignClose()"
-                    >Close</v-btn
-                >
-            </v-card-actions>
-        </v-card>
-    </div>
+                    <CountDowntimer
+                        :start-time="moment.utc(this.campaign.start).unix()"
+                        :end-text="'Campaign Started'"
+                        :interval="1000"
+                        @campaignStart="campaignStart()"
+                    >
+                        <template slot="countdown" slot-scope="scope">
+                            <span class="red--text pl-3 text-h3 justify-content"
+                                >{{ scope.props.minutes }}:{{
+                                    scope.props.seconds
+                                }}</span
+                            >
+                        </template>
+                    </CountDowntimer>
+                </div>
+            </v-card>
+            <div
+                        v-if="campaign.total_node > 0"
+                        class=" ml-auto d-inline-flex align-center"
+                    >
+                        <v-divider class="mx-4 my-0" vertical></v-divider>
+                        <p class=" pt-4 pr-3">Finished Nodes -</p>
+                        <v-progress-circular
+                            class=" pr-3"
+                            :transitionDuration="5000"
+                            :radius="25"
+                            :strokeWidth="5"
+                            :value="
+                                (campaign.b_node / campaign.total_node) * 100 ||
+                                    0.000001
+                            "
+                        >
+                            <div class="caption">
+                                {{ campaign.b_node }} /
+                                {{ campaign.total_node }}
+                            </div></v-progress-circular
+                        >
+                        <v-progress-circular
+                            class=" pr-3"
+                            :transitionDuration="5000"
+                            :radius="25"
+                            :strokeWidth="5"
+                            strokeColor="#FF3D00"
+                            :value="
+                                (campaign.r_node / campaign.total_node) * 100 ||
+                                    0.000001
+                            "
+                        >
+                            <div class="caption">
+                                {{ campaign.r_node }} /
+                                {{ campaign.total_node }}
+                            </div></v-progress-circular
+                        >
+                    </div>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
@@ -43,51 +189,28 @@ function sleep(ms) {
 }
 export default {
     data() {
-        return {
-            name: "",
-            picked: []
-        };
+        return {};
     },
 
-    created() {
-        this.$store.dispatch("getCampaignsList");
-    },
+    created() {},
 
-    methods: {
-
-       async addCampaignDone(){
-            let id = moment().format('x')
-
-            await axios({
-                method: "POST",
-                url:
-                    "/api/multicampaigns/"+id+"/"+this.name,
-                data: this.picked,
-                headers: {
-                    Authorization: "Bearer " + this.$store.state.token,
-                    Accept: "application/json",
-                    "Content-Type": "application/json"
-                }
-            })
-            this.$emit("closeAddNew")
-
-        },
-
-        addCampaignClose(){
-            this.picked = []
-            this.name = ""
-            this.$emit("closeAdd")
-
-        },
-
-    },
+    methods: {},
 
     computed: {
-        ...mapState(["campaignslist"]),
+        ...mapGetters([
+            "getCampaignById",
+            "getActiveCampaigns",
+            "getCampaignsCount",
+            "getCampaignUsersByUserId",
+            "getCampaignUsersByUserIdCount",
+            "getTotalNodeCountByCampaign",
+            "getHackingNodeCountByCampaign",
+            "getRedHackingNodeCountByCampaign"
+        ]),
 
-        list() {
-            return this.campaignslist;
-        }
+        campaign() {
+            return this.getCampaignById(this.$route.params.id);
+        },
     }
 };
 </script>
