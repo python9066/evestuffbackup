ba11396782782d78738fca89e795e5254e97f851
python9066 <python9066@gmail.com>
2020-10-26 13:13:14 +0000

    quick commit

--- resources/js/components/multicampaigns/TitleBar.vue
+++ resources/js/components/multicampaigns/TitleBar.vue
@@ -1,7 +1,7 @@
 <template>
     <v-row no-gutters v-if="this.getCampaignsCount > 1" justify="space-around">
         <v-col md="10">
-            <v-card class="pa-2 d-inline-flex align-content-center" tile width="100%">
+            <v-card class="pa-2 d-inline-flex full-width align-content-center" tile width="100%">
                 <div>
                    <p> {{ this.campaign.item_name }} in
                     {{ this.campaign.system }} - {{ this.campaign.ticker }}</p>
