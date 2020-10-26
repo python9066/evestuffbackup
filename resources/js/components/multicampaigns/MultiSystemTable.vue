74e8d02b478d31465b3f93850a7c7a23e57e6ad4
python9066 <python9066@gmail.com>
2020-10-26 16:19:05 +0000

    quick commit

--- resources/js/components/multicampaigns/MultiSystemTable.vue
+++ resources/js/components/multicampaigns/MultiSystemTable.vue
@@ -428,7 +428,7 @@ export default {
     data() {
         return {
             headers: [
-                { text: "Target", value: "text", width: "20%", align: "start" },
+                { text: "Target", value: "text", width: "18%", align: "start" },
                 { text: "NodeID", value: "node", width: "5%", align: "start" },
                 {
                     text: "Pilot",
