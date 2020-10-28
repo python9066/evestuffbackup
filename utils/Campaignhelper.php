56e0256245ea53af885e12a54bd82cbbd358962f
python9066 <python9066@gmail.com>
2020-10-28 17:31:16 +0000

    quick commit

--- utils/Campaignhelper.php
+++ utils/Campaignhelper.php
@@ -17,9 +17,7 @@ class Campaignhelper
 
     public static function update()
     {
-        $now = now();
-        $warmup = now()->modify(' -1 hour');
-        $now10 = now()->modify('-12 hours');
+
         $toDelete = Campaign::where('status_id', 10)
             ->get();
 
@@ -75,18 +73,6 @@ class Campaignhelper
                 Campaign::updateOrCreate(['id' => $id], $data);
                 $after = Campaign::where('id', $id)->get();
 
-                $warmcheck = Campaign::where('id', $id)->where('start_time','>=',$warmup)->where('warmup',0)->where('status_id', 1)->count();
-                if ($warmcheck > 0) {
-                    Campaign::where('start_time','>=', $warmup)->where('status_id', 1)->where('warmup',0)->update(['warmup' => 1]);
-                    echo "started warm up";
-                    $changed->push($id);
-                    $flag = 1;
-                }
-
-
-
-
-
                 if ($before->count() > 0) {
                     $attackers_old = $before->toArray();
                     $attackers_old = floatval($attackers_old[0]['attackers_score']);
@@ -104,7 +90,9 @@ class Campaignhelper
             }
         }
         // dd("fwefe");
-
+        $now = now();
+        $warmup = now()->modify(' -1 hour');
+        $now10 = now()->modify('-12 hours');
         $yesterday = now('-8 hours');
         $check = Campaign::where('start_time', '<=', $now)->where('status_id', 1)->count();
         if ($check > 0) {
@@ -116,14 +104,13 @@ class Campaignhelper
         }
 
 
-        $warmcheck = Campaign::where('start_time','>=',$warmup)->where('warmup',0)->where('status_id', 1)->count();
-        // $warmcheck = Campaign::where('start_time', '>=', $now)->where('start_time','<=', $warmup)->where('warmup',0)->where('status_id', 1)->count();
-        // dd( $warmcheck);
-        if ($warmcheck > 0) {
-            Campaign::where('start_time','>=', $warmup)->where('status_id', 1)->where('warmup',0)->update(['warmup' => 1]);
-            echo "started warm up";
-            $flag = 1;
-        }
+        $warmcheck = Campaign::where('start_time','>=',$warmup)->where('warmup',0)->where('status_id', 1)->get();
+            foreach($warmcheck as $warmcheck){
+                Campaign::where('id',$warmcheck['id'])->where('status_id',1)->where('warmup',0)->update(['warmup' =>1 ]);
+                $flag = 1;
+                $changed->push($warmcheck['id']);
+            };
+
         $check = Campaign::where('check', 0)->count();
 
         if ($check > 0) {
