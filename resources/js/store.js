586ee8c3b60286ff64ea09e2e784c72e98fb9a84
python9066 <python9066@gmail.com>
2020-10-28 11:27:13 +0000

    quick commit

--- resources/js/store.js
+++ resources/js/store.js
@@ -468,7 +468,7 @@ export default new Vuex.Store({
         },
 
         getsActiveCampaignById: state => payload => {
-            return state.campaignJoin.filter(campaigns => campaigns.custom_campaign_id == payload.id && campaigns.status_id == 2 && campaigns.constellation_id == payload.constellation_id);
+            return state.campaignJoin.filter(c => c.custom_campaign_id == payload.id && c.status_id == 2 && c.constellation_id == payload.constellation_id);
         },
 
 
