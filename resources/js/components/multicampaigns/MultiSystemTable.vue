88d2f583c2c53ba1f122d9fdabe91f77984ae817
python9066 <python9066@gmail.com>
2020-10-28 14:10:28 +0000

    quick commit

--- resources/js/components/multicampaigns/MultiSystemTable.vue
+++ resources/js/components/multicampaigns/MultiSystemTable.vue
@@ -849,8 +849,7 @@ export default {
 
         async addNode() {
             let node = this.nodeText.toUpperCase();
-            console.log(this.campaignActiveCountsingle)
-            if (this.campaignActiveCountsingle == true) {
+            if (this.campaignActiveCountsingle == false) {
                 console.log("kafoekofe")
                 var request = {
                     campaign_id: this.campaignJoinDrop.map(a => a.campaign_id),
