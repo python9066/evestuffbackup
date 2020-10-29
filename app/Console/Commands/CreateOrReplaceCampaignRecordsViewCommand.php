618fe1c24a07832a0b53ecd683953dc62b8c85cf
python9066 <python9066@gmail.com>
2020-10-29 13:34:36 +0000

    quick commit

--- app/Console/Commands/CreateOrReplaceCampaignRecordsViewCommand.php
+++ app/Console/Commands/CreateOrReplaceCampaignRecordsViewCommand.php
@@ -63,7 +63,7 @@ class CreateOrReplaceCampaignRecordsViewCommand extends Command
         campaigns.b_node AS 'b_node',
         campaigns.r_node AS 'r_node',
         campaigns.b_node + campaigns.r_node AS 'total_node',
-        campaigns.warmup AS 'warmup'
+        campaigns.warmup AS 'warmup',
         systems.adm AS 'adm'
         FROM campaigns
         JOIN systems ON systems.id = campaigns.system_id
