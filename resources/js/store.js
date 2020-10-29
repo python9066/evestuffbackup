0b00d9e3d46838cbf244e96db05c6aac4bba41a6
python9066 <python9066@gmail.com>
2020-10-29 12:39:29 +0000

    quick commit

--- resources/js/store.js
+++ resources/js/store.js
@@ -573,9 +573,15 @@ export default new Vuex.Store({
         getRedHackingNodeCountByMultiCampaign: state => id => {
             console.log(state.campaignsystems)
             console.log(id)
-            return state.campaignsystems.filter(
+            // return state.campaignsystems.filter(
+            //     sys => sys.custom_campaign_id == id && (sys.status_id == 7 || sys.status_id == 5)
+            // ).length
+
+            let count = state.campaignsystems.filter(
                 sys => sys.custom_campaign_id == id && (sys.status_id == 7 || sys.status_id == 5)
             ).length
+
+            console.log(count)
         },
 
 
