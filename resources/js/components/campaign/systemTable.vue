a9658d06aa34668422049651c332b5b62449eb54
python9066 <python9066@gmail.com>
2020-11-02 22:17:13 +0000

    quick commit

--- resources/js/components/campaign/systemTable.vue
+++ resources/js/components/campaign/systemTable.vue
@@ -262,7 +262,7 @@
                     hide-default-footer
                     disable-pagination
                     class="elevation-12"
-                    expand-icon="fas fa-pen"
+                    expand-icon="red fas fa-pen"
 
                 >
                     >
