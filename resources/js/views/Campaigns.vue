ec6b86acb9bde64e5e237e4d11bf0b1626c8a5c2
python9066 <python9066@gmail.com>
2020-11-04 12:51:32 +0000

    quick commit

--- resources/js/views/Campaigns.vue
+++ resources/js/views/Campaigns.vue
@@ -327,7 +327,7 @@ export default {
                 { text: "Alliance", value: "alliance" },
                 { text: "Ticker", value: "ticker", align: "start" },
                 { text: "ADM", value: "adm" },
-                { text: "Structure", value: "item_name" },
+                { text: "Structure", value: "item_name",width: "10%" },
                 {
                     text: "Start/Progress",
                     value: "start",
