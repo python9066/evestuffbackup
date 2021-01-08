2dffdbcc4205cb5bfb251ecb68d9f1bf7a3e1ad4
python9066 <python9066@gmail.com>
2021-01-08 19:38:57 +0000

    quick commit

--- app/Http/Controllers/CampaignUserController.php
+++ app/Http/Controllers/CampaignUserController.php
@@ -59,7 +59,7 @@ class CampaignUserController extends Controller
     {
         CampaignUser::find($id)->update($request->all());
         $flag = collect([
-            'flag' => 1,
+            'flag' => 4,
             'id' => $campid
         ]);
         broadcast(new CampaignSystemUpdate($flag))->toOthers();
