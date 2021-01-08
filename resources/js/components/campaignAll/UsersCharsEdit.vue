b4a326859d303f8f7596dd3972e3fbb38d3e3e52
python9066 <python9066@gmail.com>
2021-01-08 11:29:20 +0000

    quick commit

--- resources/js/components/campaignAll/UsersCharsEdit.vue
+++ resources/js/components/campaignAll/UsersCharsEdit.vue
@@ -11,7 +11,7 @@
                 v-bind="attrs"
                 v-on="on"
                 small
-                @click="(editShown = true), charEditForm(item)"
+                @click="(editShown = true, charEditForm(item))"
             >
                 fas fa-edit
             </v-icon>
