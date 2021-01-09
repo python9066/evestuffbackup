c7bebf656b0bb162b06b8bb5fee17c84a2963e42
python9066 <python9066@gmail.com>
2021-01-09 20:54:06 +0000

    quick commit

--- utils/Campaignhelper.php
+++ utils/Campaignhelper.php
@@ -96,7 +96,7 @@ class Campaignhelper
                     };
                 } else {
                     $constellation = System::where('id', $var['solar_system_id'])->value('constellation_id');
-                    $systems = System::where('constellation_id', $constellation)->value('id')->get();
+                    $systems = System::where('constellation_id', $constellation)->value('id');
                     dd($systems);
                 };
             }
