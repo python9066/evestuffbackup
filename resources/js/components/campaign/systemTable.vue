2b6d814863b6ff2af25e07a536334b7e6c0eb124
python9066 <python9066@gmail.com>
2021-01-11 11:07:47 +0000

    quick commit

--- resources/js/components/campaign/systemTable.vue
+++ resources/js/components/campaign/systemTable.vue
@@ -536,7 +536,7 @@ export default {
         checkClick() {
             this.test1 = this.CampaignSolaSystem[0]["id"];
             var user = this.$store.state.user_id;
-            var timeStamp = new date();
+            var timeStamp = Date();
             console.log(timeStamp);
         },
 
