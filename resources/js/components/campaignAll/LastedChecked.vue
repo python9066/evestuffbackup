787404da44ec005ba669033945cdc6b0714f4058
python9066 <python9066@gmail.com>
2021-01-11 12:48:45 +0000

    quick commit

--- resources/js/components/campaignAll/LastedChecked.vue
+++ resources/js/components/campaignAll/LastedChecked.vue
@@ -1,7 +1,12 @@
 <template>
     <div class=" d-inline-flex align-items-md-center ">
         <div v-if="$can('super')">
-            <v-btn class="mr-4 ml-4" color="green" small @click="checkClick()">
+            <v-btn
+                class="pr-4 pl-4 pb-4 "
+                color="green"
+                small
+                @click="checkClick()"
+            >
                 <v-icon small left dark>
                     fas fa-search-location
                 </v-icon>
