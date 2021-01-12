9ee5bae9f44cd10ca5f79cae725d509bfbf2a60d
python9066 <python9066@gmail.com>
2021-01-12 15:55:07 +0000

    quick commit

--- resources/js/components/campaign/systemTable.vue
+++ resources/js/components/campaign/systemTable.vue
@@ -407,7 +407,7 @@
             </v-card-text>
             <v-spacer></v-spacer>
             <v-card-actions max-width class=" pt-4 d-inline-flex">
-                <div>
+                <div max-width>
                     <div class=" d-inline-flex">
                         <LastedChecked :CampaignSolaSystem="CampaignSolaSystem">
                         </LastedChecked>
