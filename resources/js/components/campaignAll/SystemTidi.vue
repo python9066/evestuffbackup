f27a959d9679c5a6b9e3fd312dd5fe1bd5dad9be
python9066 <python9066@gmail.com>
2021-01-12 22:51:31 +0000

    quick commit

--- resources/js/components/campaignAll/SystemTidi.vue
+++ resources/js/components/campaignAll/SystemTidi.vue
@@ -1,58 +1,62 @@
 <template>
     <div class=" pr-4 h4 align-items-baseline justify-sm-end">
-        System TiDi:
-        <span class="green--text font-weight-bold"
-            >{{ CampaignSolaSystem[0]["tidi"] }}%</span
-        >
-        <v-menu :close-on-content-click="false" :value="tidiShow">
-            <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                    class="pb-1"
-                    v-bind="attrs"
-                    v-on="on"
-                    @click="(tidiShow = true), (tidiEdit = null)"
-                    icon
-                    color="warning"
-                >
-                    <v-icon>fas fa-edit</v-icon>
-                </v-btn>
-            </template>
+        <div>
+            System TiDi:
+            <span class="green--text font-weight-bold"
+                >{{ CampaignSolaSystem[0]["tidi"] }}%</span
+            >
+            <v-menu :close-on-content-click="false" :value="tidiShow">
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        class="pb-1"
+                        v-bind="attrs"
+                        v-on="on"
+                        @click="(tidiShow = true), (tidiEdit = null)"
+                        icon
+                        color="warning"
+                    >
+                        <v-icon>fas fa-edit</v-icon>
+                    </v-btn>
+                </template>
 
-            <template>
-                <v-card tile min-height="150px">
-                    <v-card-title class=" pb-0">
-                        <v-text-field
-                            v-model="tidiEdit"
-                            label="Tidi %"
-                            autofocus
-                            v-mask="'###'"
-                            :placeholder="CampaignSolaSystem[0]['tidi']"
-                            @keyup.enter="tidiShow = false"
-                            @keyup.esc="(tidiShow = false), (tidiEdit = null)"
-                        ></v-text-field>
-                    </v-card-title>
-                    <v-card-text>
-                        <v-btn
-                            icon
-                            fixed
-                            left
-                            color="success"
-                            @click="tidiShow = false"
-                            ><v-icon>fas fa-check</v-icon></v-btn
-                        >
+                <template>
+                    <v-card tile min-height="150px">
+                        <v-card-title class=" pb-0">
+                            <v-text-field
+                                v-model="tidiEdit"
+                                label="Tidi %"
+                                autofocus
+                                v-mask="'###'"
+                                :placeholder="CampaignSolaSystem[0]['tidi']"
+                                @keyup.enter="tidiShow = false"
+                                @keyup.esc="
+                                    (tidiShow = false), (tidiEdit = null)
+                                "
+                            ></v-text-field>
+                        </v-card-title>
+                        <v-card-text>
+                            <v-btn
+                                icon
+                                fixed
+                                left
+                                color="success"
+                                @click="tidiShow = false"
+                                ><v-icon>fas fa-check</v-icon></v-btn
+                            >
 
-                        <v-btn
-                            fixed
-                            right
-                            icon
-                            color="warning"
-                            @click="(tidiShow = false), (tidiEdit = null)"
-                            ><v-icon>fas fa-times</v-icon></v-btn
-                        >
-                    </v-card-text>
-                </v-card>
-            </template>
-        </v-menu>
+                            <v-btn
+                                fixed
+                                right
+                                icon
+                                color="warning"
+                                @click="(tidiShow = false), (tidiEdit = null)"
+                                ><v-icon>fas fa-times</v-icon></v-btn
+                            >
+                        </v-card-text>
+                    </v-card>
+                </template>
+            </v-menu>
+        </div>
     </div>
 </template>
 
