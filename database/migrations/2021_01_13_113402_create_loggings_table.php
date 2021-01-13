8a1a9d253063622053ec2651b8a18f597b7276c5
python9066 <python9066@gmail.com>
2021-01-13 15:27:02 +0000

    quick commit

--- database/migrations/2021_01_13_113402_create_loggings_table.php
+++ database/migrations/2021_01_13_113402_create_loggings_table.php
@@ -18,7 +18,7 @@ class CreateLoggingsTable extends Migration
             $table->id();
             $table->foreignId('campaign_id');
             $table->foreignId('campaign_sola_systems_id');
-            $table->foreignId('campaign_systems_id')->nullable();
+            $table->string('campaign_systems_id', 4)->nullable();
             $table->foreignId('user_id');
             $table->foreignId('logging_type_id')->nullable();
             $table->text('text')->nullable();
