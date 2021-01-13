e0d73dda122df70b8f5f09049a3afcdee481b8e5
python9066 <python9066@gmail.com>
2021-01-13 21:39:13 +0000

    quick commit

--- resources/js/components/multicampaigns/MultiSystemTable.vue
+++ resources/js/components/multicampaigns/MultiSystemTable.vue
@@ -684,7 +684,7 @@ export default {
 
             this.$store.dispatch("getCampaignSystemsRecords");
             this.$store.dispatch("getCampaignUsersRecords", this.campaign_id);
-            request = null;
+            var request = null;
             request = {
                 campaign_id: this.campaign_id,
                 campaign_systems_id: item.node_id,
