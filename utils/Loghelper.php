7b86fcd2be130d3f3d3beba26e12cb82c8f191af
python9066 <python9066@gmail.com>
2021-01-13 11:20:33 +0000

    quick commit

---
+++ utils/Loghelper.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace utils\Timerhelper;
+
+use App\Models\Structure;
+use App\Models\System;
+use GuzzleHttp\Client;
+use utils\Helper\Helper;
+use GuzzleHttp\Utils;
+
+use function GuzzleHttp\json_decode;
+
+class Timerhelper
+{
+
+    public static function update()
+    {
+    }
+}
