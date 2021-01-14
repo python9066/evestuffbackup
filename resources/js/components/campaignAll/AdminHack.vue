5f066499cb73eb6604db614ab642bcf124340f66
python9066 <python9066@gmail.com>
2021-01-14 11:36:16 +0000

    quick commit

--- resources/js/components/campaignAll/AdminHack.vue
+++ resources/js/components/campaignAll/AdminHack.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
-        <v-btn :v-if="showAddIcon" icon color="green darken-3">
-            <v-icon small>fas fa-plus</v-icon></v-btn
-        >
-        <v-btn :v-if="showRemoveIcon" icon color="red darken-3">
-            <v-icon small>fas fa-trash-alt</v-icon></v-btn
-        >
+        <span :v-if="showAddIcon">
+            <v-btn icon color="green darken-3">
+                <v-icon small>fas fa-plus</v-icon></v-btn
+            >
+        </span>
+        <span :v-if="showRemoveIcon">
+            <v-btn icon color="red darken-3">
+                <v-icon small>fas fa-trash-alt</v-icon></v-btn
+            >
+        </span>
         <!-- <v-card
         tile
         min-width="1400px"
