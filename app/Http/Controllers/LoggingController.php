2139efd9f1abe66c87a4fef58a1cf5eaee81ff37
python9066 <python9066@gmail.com>
2021-01-15 14:56:06 +0000

    quick commit

--- app/Http/Controllers/LoggingController.php
+++ app/Http/Controllers/LoggingController.php
@@ -40,9 +40,9 @@ class LoggingController extends Controller
             'id' => $campid,
         ]);
         broadcast(new CampaignSystemUpdate($flag));
-        $systemname = Campaign::where('id', $log->campaign_id)->with('system')->get();
+        $systemname = Campaign::where('id', $log->campaign_id)->with('system')->with('structure')->get();
         // $test = $systemname->system;
-        dd($request, $systemname);
+        dd($systemname);
     }
 
     public function nodeDelete(Request $request, $campid)
