0c282d4bd99334f0505b3e2508c102e26d292723
python9066 <python9066@gmail.com>
2021-01-15 22:36:38 +0000

    quick commit

--- app/Http/Controllers/LoggingController.php
+++ app/Http/Controllers/LoggingController.php
@@ -45,6 +45,7 @@ class LoggingController extends Controller
 
         $campaignname = Helper::campaignName($log->campaign_id);
         $sola_name = CampaignSolaSystem::where('system_id', $request->campaign_sola_systems_id)->first()->systems->system_name;
+        $sola_name = $sola_name->systems->system_name;
         dd($sola_name);
         $text = $log->user->name . " added node " . $request->campaign_systems_id . " for the " . $campaignname['campaign_name'] . " at " . $log->created_at;
         $log->update(['campaign_name' => $campaignname['campaign_name'], 'sola_system_name' => $campaignname['system_name'], 'text' => $text]);
