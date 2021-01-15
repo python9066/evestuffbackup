bd71767f34506329934f5a280f273b7c9440e531
python9066 <python9066@gmail.com>
2021-01-15 02:54:36 +0000

    quick commit

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Events\StationNew;
 use App\Events\TowerNew;
+use App\Models\Logging;
+use App\Models\test;
 use Illuminate\Http\Request;
 use utils\Helper\Helper;
 use utils\Notificationhelper\Notifications;
@@ -18,6 +20,27 @@ class testController extends Controller
 
     public function notifications(Request $request)
     {
+
+        Logging::create(['campaign_id' => 1, 'campaign_sola_system_id' => 1, 'user_id' => 1]);
+        test($request)
+        // $data =  $request->toArray();
+        // $flag = Notifications::test($data);
+
+        // if ($flag['stationflag'] == 1) {
+        //     broadcast(new StationNew($flag['stationflag']))->toOthers();
+        // }
+
+        // if ($flag['tower'] == 1) {
+        //     broadcast(new TowerNew($flag['towerflag']))->toOthers();
+        // }
+
+
+    }
+
+    public function test($data)
+    {
+
+        Logging::create(['campaign_id' => 1, 'campaign_sola_system_id' => 1, 'user_id' => 1,'text' =>$data]);
         // $data =  $request->toArray();
         // $flag = Notifications::test($data);
 
