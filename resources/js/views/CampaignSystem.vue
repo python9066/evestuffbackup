858ec659b0277a8eb5418b52bcd00f3270371bf7
python9066 <python9066@gmail.com>
2021-01-17 13:22:44 +0000

    quick commit

--- resources/js/views/CampaignSystem.vue
+++ resources/js/views/CampaignSystem.vue
@@ -482,6 +482,9 @@ export default {
         if (this.$store.getters.getCampaignsCount == 0) {
             await this.$store.dispatch("getCampaigns");
         }
+        if (this.campaign == undefined) {
+            await sleep(5000);
+        }
         console.log(this.campaign);
         this.campaignId = this.campaign.id;
         Echo.private("campaignsystem." + this.campaign.id).listen(
