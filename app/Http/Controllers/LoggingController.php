0b1b729a72d4861653b46c3b686fa058813cdbdb
python9066 <python9066@gmail.com>
2021-01-17 01:25:19 +0000

    quick commit

--- app/Http/Controllers/LoggingController.php
+++ app/Http/Controllers/LoggingController.php
@@ -26,7 +26,7 @@ class LoggingController extends Controller
         $data = [];
         $logs = Logging::all();
         foreach ($logs as $log) {
-            dd($log);
+            // dd($log);
             $data1 = null;
             $data1 = [
                 'id' => $log['id'],
