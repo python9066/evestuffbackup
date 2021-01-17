6d27ecdfb1f23ab730163e3c221e034362928bee
python9066 <python9066@gmail.com>
2021-01-17 14:45:21 +0000

    quick commit

--- resources/js/views/CampaignSystem.vue
+++ resources/js/views/CampaignSystem.vue
@@ -479,6 +479,7 @@ export default {
     },
 
     async created() {
+        console.log(this.campaign);
         this.campaignId = this.campaign.id;
         Echo.private("campaignsystem." + this.campaign.id).listen(
             "CampaignSystemUpdate",
