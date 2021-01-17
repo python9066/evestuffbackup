ea5f5ed509a5f04c6cfdbbc461c659a0e775f577
python9066 <python9066@gmail.com>
2021-01-17 14:58:56 +0000

    quick commit

--- resources/js/views/CampaignSystem.vue
+++ resources/js/views/CampaignSystem.vue
@@ -552,16 +552,12 @@ export default {
     async beforeCreate() {},
 
     async mounted() {
-        await this.$store.dispatch("getCampaignSolaSystems");
-        // if (this.$store.getters.getCampaignsCount == 0) {
-        //     await this.$store.dispatch("getCampaigns");
-        // }
-        // console.log(this.$route.params.id)
-        await this.getSystems(this.campaign.constellation_id);
-        await this.$store.dispatch("getCampaignUsersRecords", this.campaign.id);
-        await this.$store.dispatch("getCampaignSystemsRecords");
-        await this.$store.dispatch("getUsersChars", this.$store.state.user_id);
-        await this.loadCampaignlogs();
+        // await this.$store.dispatch("getCampaignSolaSystems");
+        // await this.getSystems(this.campaign.constellation_id);
+        // await this.$store.dispatch("getCampaignUsersRecords", this.campaign.id);
+        // await this.$store.dispatch("getCampaignSystemsRecords");
+        // await this.$store.dispatch("getUsersChars", this.$store.state.user_id);
+        // await this.loadCampaignlogs();
     },
     methods: {
         checkAddUser() {
