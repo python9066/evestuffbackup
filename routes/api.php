3ae6d29caccecba71d3a7a5bd9803b25eb3a4a60
python9066 <python9066@gmail.com>
2021-01-17 12:43:32 +0000

    quick commit

--- routes/api.php
+++ routes/api.php
@@ -111,5 +111,5 @@ Route::middleware('auth:api')->group(function () {
     Route::put('/checklastedchecked/{campid}', 'LoggingController@lastchecked');
     Route::put('/checkscout/{campid}', 'LoggingController@systemscout');
     Route::put('/checkaddremovechar/{campid}', 'LoggingController@addremovechar');
-    Route::get('/checkcampaign/{$campid}', 'LoggingController@logCampaign');
+    Route::get('/checkcampaign/{campid}', 'LoggingController@logCampaign');
 });
