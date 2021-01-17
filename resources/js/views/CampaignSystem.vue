0264cea7a78d0ecf1d6b5e71fbd6b10e27ead778
python9066 <python9066@gmail.com>
2021-01-17 15:05:45 +0000

    quick commit

--- resources/js/views/CampaignSystem.vue
+++ resources/js/views/CampaignSystem.vue
@@ -382,18 +382,18 @@
             </v-card>
         </v-overlay>
         <v-overlay z-index="0" :value="overlay" min-width="1000px">
-            <!-- <UsersChars
+            <UsersChars
                 :campaign_id="campaign.id"
                 @closeAddChar="overlay = false"
             >
-            </UsersChars> -->
+            </UsersChars>
         </v-overlay>
         <v-overlay z-index="0" :value="showNotes">
-            <ShowNotes
+            <!-- <ShowNotes
                 :campaign_id="campaign.id"
                 @closeNotes="showNotes = false"
             >
-            </ShowNotes>
+            </ShowNotes> -->
         </v-overlay>
 
         <v-overlay z-index="0" :value="showAdd">
