71ae70ab9ca001332af6e3e65c393378b02dcc62
python9066 <python9066@gmail.com>
2021-01-18 14:49:40 +0000

    quick commit

--- resources/js/views/MultiCampaignSystem.vue
+++ resources/js/views/MultiCampaignSystem.vue
@@ -782,13 +782,13 @@ export default {
             return this.getsCampaignById(this.campaignId);
         },
 
-        customCampaign() {
-            var d = this.getMultiCampaignName(this.$route.params.id);
-            this.load = 1;
-            this.logName = d[0]["name"];
-            console.log(this.logName);
-            return d;
-        },
+        // customCampaign() {
+        //     var d = this.getMultiCampaignName(this.$route.params.id);
+        //     this.load = 1;
+        //     this.logName = d[0]["name"];
+        //     console.log(this.logName);
+        //     return d;
+        // },
 
         campaignWarmup() {
             let count = this.getsCampaignById(this.campaignId).filter(
