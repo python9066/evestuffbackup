769d118cc615a7dd1dbbe8392bbecadadfb4d085
python9066 <python9066@gmail.com>
2021-01-18 14:55:38 +0000

    quick commit

--- resources/js/views/MultiCampaignSystem.vue
+++ resources/js/views/MultiCampaignSystem.vue
@@ -439,7 +439,7 @@ export default {
             let d = await this.$store.getters.getMultiCampaignName(
                 this.$route.params.id
             );
-            console.log(d.name);
+            console.log(d[0]["name"]);
             this.logName = d[0]["name"];
         },
 
