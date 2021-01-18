d424999d2abc46ab80a81779653573cfe750a8f9
python9066 <python9066@gmail.com>
2021-01-18 11:33:31 +0000

    quick commit

--- resources/js/views/CampaignSystem.vue
+++ resources/js/views/CampaignSystem.vue
@@ -413,8 +413,7 @@
             >
             </AdminHackUserTable>
         </v-overlay>
-        <v-overlay z-index="5" :value="showLog">
-            <!-- campaignAll/admin/UserTable.vue -->
+        <!-- <v-overlay z-index="5" :value="showLog">
             <CampaignLogging
                 v-if="$can('view_campaign_logs')"
                 @closeLog="showLog = false"
@@ -422,7 +421,7 @@
                 :campaign="campaign"
             >
             </CampaignLogging>
-        </v-overlay>
+        </v-overlay> -->
     </div>
 </template>
 <!-- {{ $route.params.id }} - {{ test }} -  -->
