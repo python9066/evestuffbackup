6ee3d84e59421a8745367e0ebc9988f64e4a1c51
python9066 <python9066@gmail.com>
2021-01-19 00:06:21 +0000

    quick commit

--- utils/Campaignhelper.php
+++ utils/Campaignhelper.php
@@ -124,7 +124,8 @@ class Campaignhelper
 
         $warmchecks = Campaign::where('warmup', 0)->where('status_id', 1)->get();
         foreach ($warmchecks as $warmcheck) {
-            $test = strtotime($warmcheck->start_time)  - now();
+            $time = $warmcheck->start_time;
+            $test = strtotime($time)  - now();
             dd($test);
             Campaign::where('id', $warmcheck['id'])->where('status_id', 1)->where('warmup', 0)->update(['warmup' => 1]);
             $flag = 1;
