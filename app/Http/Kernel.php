10bd206d48235ad0440b64f4af9b23e3124748bd
python9066 <python9066@gmail.com>
2021-01-19 01:33:14 +0000

    quick commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,7 +3,6 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
-use MyApp\Http\Middleware\HttpsProtocol;
 
 class Kernel extends HttpKernel
 {
@@ -23,7 +22,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
 
-        HttpsProtocol::class
     ];
 
     /**
