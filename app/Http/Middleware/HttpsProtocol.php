cda8c2bf1df41eb7c35abfbce4f1e51a74035227
python9066 <python9066@gmail.com>
2021-01-19 01:37:03 +0000

    quick commit

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -1,16 +1,22 @@
 <?php
 
-namespace MyApp\Http\Middleware;
+namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\App;
 
-class HttpsProtocol
+class HttpsProtocolMiddleware
 {
-
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
     public function handle($request, Closure $next)
     {
-        if (!$request->secure() && App::environment() === 'production') {
+        if (!$request->secure() && app()->environment('production')) {
             return redirect()->secure($request->getRequestUri());
         }
 
