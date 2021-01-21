3941d76132779e4254f3e4027b055da3b7d2d472
python9066 <python9066@gmail.com>
2021-01-21 11:04:31 +0000

    quick commit

--- resources/js/views/AdminPanel.vue
+++ resources/js/views/AdminPanel.vue
@@ -354,7 +354,7 @@ export default {
 
             request = null;
             request = {
-                roleId: this.userAddRole,
+                roleId: this.userAddRoleText,
                 userId: item.id,
                 user_id: this.$store.state.user_id,
                 type: 15
