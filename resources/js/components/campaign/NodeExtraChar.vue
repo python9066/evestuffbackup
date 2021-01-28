0673136e8dbdb92dee217f4dfef01fd15ef48314
python9066 <python9066@gmail.com>
2021-01-28 11:25:49 +0000

    quick commit

--- resources/js/components/campaign/NodeExtraChar.vue
+++ resources/js/components/campaign/NodeExtraChar.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <v-btn v-if="showAddIcon" icon color="green darken-3" @click="showAdd">
+        <v-btn icon color="green darken-3" @click="showAdd">
             <v-icon small>fas fa-plus</v-icon></v-btn
         >
     </div>
