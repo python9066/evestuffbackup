2516031b596af6b6c3051a18fc14ee4c1015cc78
python9066 <python9066@gmail.com>
2021-01-29 12:43:28 +0000

    quick commit

--- app/Http/Controllers/NodeJoinsController.php
+++ app/Http/Controllers/NodeJoinsController.php
@@ -34,8 +34,8 @@ class NodeJoinsController extends Controller
             $user_id = $node->campaign_user_id;
             $campaign_system_status_id = $node->campaign_system_status_id;
             $CampaignSystem = CampaignSystem::where('id', $id)->get();
-            dd($CampaignSystem);
-            $oldUser_id = $CampaignSystem->campaign_user_id;
+            $oldUser_id = $CampaignSystem->value('campaign_user_id');
+            dd($oldUser_id);
             CampaignUser::where('id', $CampaignSystem->campaign_user_id)->update(['campaign_system_id' => null, 'status_id' => 3]);
             $CampaignSystem->update(['campaign_user_id' => $user_id, 'campaign_system_status_id' => $campaign_system_status_id]);
         } else {
