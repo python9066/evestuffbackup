1867214836737a840e9b111f9ccc778d3a0155b8
python9066 <python9066@gmail.com>
2021-01-29 14:15:52 +0000

    quick commit

--- resources/js/components/random/TidiCalc.vue
+++ resources/js/components/random/TidiCalc.vue
@@ -9,15 +9,155 @@
             class=" d-lg-inline-block justify-space-between align-center "
         >
             <div>
-                Logs for Roles Changes
+                Tidi Calculator
             </div>
             <div>
-                <v-text-field
-                    append-icon="mdi-magnify"
-                    label="Search"
-                    single-line
-                    hide-details
-                ></v-text-field>
+                <v-menu :close-on-content-click="false" :value="timerShown">
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-chip
+                            v-bind="attrs"
+                            v-on="on"
+                            pill
+                            :outlined="pillOutlined(item)"
+                            @click="timerShown = true"
+                            small
+                            color="warning"
+                        >
+                            Add Time
+                        </v-chip>
+                    </template>
+
+                    <template>
+                        <v-card tile min-height="150px">
+                            <v-card-title class=" pb-0">
+                                <v-text-field
+                                    v-model="hackTime"
+                                    label="Hack Time mm:ss"
+                                    v-mask="'##:##'"
+                                    autofocus
+                                    placeholder="mm:ss"
+                                    @keyup.enter="
+                                        (timerShown = false), addHacktime(item)
+                                    "
+                                    @keyup.esc="
+                                        (timerShown = false), (hackTime = null)
+                                    "
+                                ></v-text-field>
+                            </v-card-title>
+                            <v-card-text>
+                                <v-btn
+                                    icon
+                                    fixed
+                                    left
+                                    color="success"
+                                    @click="
+                                        (timerShown = false), addHacktime(item)
+                                    "
+                                    ><v-icon>fas fa-check</v-icon></v-btn
+                                >
+
+                                <v-btn
+                                    fixed
+                                    right
+                                    icon
+                                    color="warning"
+                                    @click="
+                                        (timerShown = false), (hackTime = null)
+                                    "
+                                    ><v-icon>fas fa-times</v-icon></v-btn
+                                >
+                            </v-card-text>
+                        </v-card>
+                    </template>
+                </v-menu>
+                <CountDowntimer
+                    :start-time="moment.utc(item.end).unix()"
+                    :end-text="endText(item)"
+                    :interval="1000"
+                >
+                    <template slot="countdown" slot-scope="scope">
+                        <span :class="hackCountDownTextColor(item)"
+                            ><span v-if="scope.props.hours > 0"
+                                >{{ scope.props.hours }}:</span
+                            >{{ scope.props.minutes }}:{{
+                                scope.props.seconds
+                            }}</span
+                        >
+                        <v-menu
+                            :close-on-content-click="false"
+                            :value="timerShown"
+                        >
+                            <template v-slot:activator="{ on, attrs }">
+                                <v-btn
+                                    v-if="checkHackUserEdit(item)"
+                                    v-bind="attrs"
+                                    v-on="on"
+                                    @click="
+                                        (timerShown = true), (hackTime = null)
+                                    "
+                                    icon
+                                    color="warning"
+                                >
+                                    <v-icon x-small>fas fa-edit</v-icon>
+                                </v-btn>
+                            </template>
+
+                            <template>
+                                <v-card tile min-height="150px">
+                                    <v-card-title class=" pb-0">
+                                        <v-text-field
+                                            v-model="hackTime"
+                                            label="Hack Time mm:ss"
+                                            autofocus
+                                            v-mask="'##:##'"
+                                            placeholder="mm:ss"
+                                            @keyup.enter="
+                                                (timerShown = false),
+                                                    addHacktime(item)
+                                            "
+                                            @keyup.esc="
+                                                (timerShown = false),
+                                                    (hackTime = null)
+                                            "
+                                        ></v-text-field>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        <v-btn
+                                            icon
+                                            fixed
+                                            left
+                                            color="success"
+                                            @click="
+                                                (timerShown = false),
+                                                    addHacktime(item)
+                                            "
+                                            ><v-icon
+                                                >fas fa-check</v-icon
+                                            ></v-btn
+                                        >
+
+                                        <v-btn
+                                            fixed
+                                            right
+                                            icon
+                                            color="warning"
+                                            @click="
+                                                (timerShown = false),
+                                                    (hackTime = null)
+                                            "
+                                            ><v-icon
+                                                >fas fa-times</v-icon
+                                            ></v-btn
+                                        >
+                                    </v-card-text>
+                                </v-card>
+                            </template>
+                        </v-menu>
+                    </template>
+                    <template slot="end-text" slot-scope="scope">
+                        <span>{{ scope.props.endText }}</span>
+                    </template>
+                </CountDowntimer>
             </div>
         </v-card-title>
         <v-card-text> </v-card-text>
@@ -37,7 +177,12 @@ export default {
     props: {},
     data() {
         return {
-            overlay: true
+            overlay: true,
+            timerShown: false,
+            hackTime: {
+                mm: "",
+                ss: ""
+            }
         };
     },
 
