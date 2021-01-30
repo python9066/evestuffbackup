3ee9e5f34bf67d3fa9f9051bc94380c0c12b8ce7
python9066 <python9066@gmail.com>
2021-01-30 11:49:28 +0000

    quick commit

--- resources/js/components/campaignAll/admin/UserTable.vue
+++ resources/js/components/campaignAll/admin/UserTable.vue
@@ -150,70 +150,71 @@ export default {
                         "Content-Type": "application/json"
                     }
                 });
-            } else {
-                var data = {
-                    id: this.nodeItem.id,
-                    user_id: item.id,
-                    site_id: this.$store.state.user_id,
-                    user_name: item.char_name,
-                    main_name: item.main_name,
-                    user_ship: item.ship,
-                    user_link: item.link
-                };
-
-                var request = {
-                    campaign_user_id: item.id
-                };
-                this.$store.dispatch("updateCampaignSystem", data);
-
-                data = null;
-                data = {
-                    id: item.id,
-                    campaign_system_id: this.nodeItem.custom_campaign_id,
-                    node_id: this.nodeItem.node,
-                    system_id: this.nodeItem.system_id,
-                    system_name: this.nodeItem.system_name,
-                    status_id: 4,
-                    user_status_name: "Hacking"
-                };
-
-                var request1 = {
-                    campaign_system_id: this.nodeItem.id,
-                    system_id: this.nodeItem.system_id,
-                    status_id: 4
-                };
-                this.$store.dispatch("updateCampaignUsers", data);
-
-                axios({
-                    method: "put", //you can set what request you want to be
-                    url:
-                        "/api/campaignsystems/" +
-                        this.nodeItem.id +
-                        "/" +
-                        this.nodeItem.campaign_id,
-                    data: request,
-                    headers: {
-                        Authorization: "Bearer " + this.$store.state.token,
-                        Accept: "application/json",
-                        "Content-Type": "application/json"
-                    }
-                });
-
-                axios({
-                    method: "put", //you can set what request you want to be
-                    url:
-                        "/api/campaignusers/" +
-                        item.id +
-                        "/" +
-                        this.nodeItem.custom_campaign_id,
-                    data: request1,
-                    headers: {
-                        Authorization: "Bearer " + this.$store.state.token,
-                        Accept: "application/json",
-                        "Content-Type": "application/json"
-                    }
-                });
             }
+            // else {
+            //     var data = {
+            //         id: this.nodeItem.id,
+            //         user_id: item.id,
+            //         site_id: this.$store.state.user_id,
+            //         user_name: item.char_name,
+            //         main_name: item.main_name,
+            //         user_ship: item.ship,
+            //         user_link: item.link
+            //     };
+
+            //     var request = {
+            //         campaign_user_id: item.id
+            //     };
+            //     this.$store.dispatch("updateCampaignSystem", data);
+
+            //     data = null;
+            //     data = {
+            //         id: item.id,
+            //         campaign_system_id: this.nodeItem.custom_campaign_id,
+            //         node_id: this.nodeItem.node,
+            //         system_id: this.nodeItem.system_id,
+            //         system_name: this.nodeItem.system_name,
+            //         status_id: 4,
+            //         user_status_name: "Hacking"
+            //     };
+
+            //     var request1 = {
+            //         campaign_system_id: this.nodeItem.id,
+            //         system_id: this.nodeItem.system_id,
+            //         status_id: 4
+            //     };
+            //     this.$store.dispatch("updateCampaignUsers", data);
+
+            //     axios({
+            //         method: "put", //you can set what request you want to be
+            //         url:
+            //             "/api/campaignsystems/" +
+            //             this.nodeItem.id +
+            //             "/" +
+            //             this.nodeItem.campaign_id,
+            //         data: request,
+            //         headers: {
+            //             Authorization: "Bearer " + this.$store.state.token,
+            //             Accept: "application/json",
+            //             "Content-Type": "application/json"
+            //         }
+            //     });
+
+            //     axios({
+            //         method: "put", //you can set what request you want to be
+            //         url:
+            //             "/api/campaignusers/" +
+            //             item.id +
+            //             "/" +
+            //             this.nodeItem.custom_campaign_id,
+            //         data: request1,
+            //         headers: {
+            //             Authorization: "Bearer " + this.$store.state.token,
+            //             Accept: "application/json",
+            //             "Content-Type": "application/json"
+            //         }
+            //     });
+            // }
         }
     },
 
