6c4bf96afa14164187defe399abd163af5115696
python9066 <python9066@gmail.com>
2021-01-30 16:11:41 +0000

    quick commit

--- resources/js/components/campaign/JoinNodeTable.vue
+++ resources/js/components/campaign/JoinNodeTable.vue
@@ -11,6 +11,72 @@
             disable-pagination
             class="pl-15"
         >
+            <template v-slot:item.user_name="{ item }">
+                <div class=" d-inline-flex align-items-center">
+                    <div class=" d-inline-flex align-items-center">
+                        {{ item.user_name }}
+                        <v-btn
+                            v-if="checkShowAddRemove(item)"
+                            icon
+                            @click="
+                                (item.user_name = null),
+                                    (item.main_name = null),
+                                    removeCharNode(item)
+                            "
+                            color="orange darken-3"
+                        >
+                            <v-icon small>fas fa-trash-alt</v-icon></v-btn
+                        >
+                        <NodeExtraChar
+                            v-if="$can('super')"
+                            :item="item"
+                        ></NodeExtraChar>
+                    </div>
+                    <AdminHack
+                        v-if="$can('campaigns_admin_access')"
+                        :item="item"
+                        @openAdd="openAdd($event)"
+                    ></AdminHack>
+                </div>
+            </template>
+            <template v-slot:item.status_name="{ item }">
+                <v-menu offset-y>
+                    <template v-slot:activator="{ on, attrs }">
+                        <div>
+                            <v-chip
+                                v-bind="attrs"
+                                v-on="on"
+                                pill
+                                small
+                                :color="pillColor(item)"
+                            >
+                                {{ item.status_name }}
+                            </v-chip>
+                        </div>
+                    </template>
+
+                    <v-list>
+                        <v-list-item
+                            v-for="(list, index) in dropdown_edit"
+                            :key="index"
+                            @click="
+                                (item.status_id = list.value),
+                                    (item.status_name = list.title),
+                                    statusClick(item)
+                            "
+                        >
+                            <v-list-item-title>{{
+                                list.title
+                            }}</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
+            </template>
+            <template v-slot:item.user_ship="{ item }">
+                <span v-if="item.user_name != null">
+                    {{ item.user_ship }} - T{{ item.user_link }}
+                </span>
+            </template>
         </v-data-table>
     </div>
 </template>
@@ -45,17 +111,126 @@ export default {
                     align: "center"
                 },
                 { text: "", value: "actions", width: "5%", align: "end" }
-            ]
+            ],
+
+            dropdown_edit: [
+                { title: "New", value: 1 },
+                { title: "Warm up", value: 2 },
+                { title: "Hacking", value: 3 },
+                { title: "Friendly Hacking", value: 8 },
+                { title: "Pushed off", value: 6 }
+            ],
+            expanded: [],
+            singleExpand: false
         };
     },
 
-    expanded: [],
-    singleExpand: false,
+    methods: {
+        pillColor(item) {
+            if (item.status_id == 1) {
+                return "deep-orange lighten-1";
+            }
+            if (item.status_id == 2) {
+                return "lime darken-4";
+            }
+            if (item.status_id == 3 || item.status_id == 8) {
+                return "green darken-3";
+            }
+            if (item.status_id == 4) {
+                return "green accent-4";
+            }
+            if (item.status_id == 5) {
+                return "red darken-4";
+            }
+            if (item.status_id == 6) {
+                return "#FF5EEA";
+            }
+            if (item.status_id == 7) {
+                return "#801916";
+            }
+            if (item.status_id == 9) {
+                return "#9C9C9C";
+            }
+        },
 
-    methods: {},
+        checkShowAddRemove(item) {
+            if (
+                item.user_name != null &&
+                this.charCount != 0 &&
+                item.status_id != 4 &&
+                item.status_id != 5 &&
+                item.status_id != 7 &&
+                item.status_id != 8
+            ) {
+                return true;
+            } else if (this.$can("campaigns_admin_access")) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+
+        removeCharNode(item) {
+            // var userId = item.user_id;
+            // item.user_id = null;
+            // item.user_ship = null;
+            // item.user_link = null;
+            // this.$store.dispatch("updateCampaignSystem", item);
+            // var data = {
+            //     id: userId,
+            //     node_id: null,
+            //     status_id: 3,
+            //     user_status_name: "Ready to go"
+            // };
+            // if (userId != null) {
+            //     this.$store.dispatch("updateCampaignUsers", data);
+            // }
+            // var request = null;
+            // if (item.status_id == 4 || item.status_id == 5) {
+            //     request = {
+            //         campaign_user_id: null,
+            //         campaign_system_status_id: item.status_id
+            //     };
+            // } else if (
+            //     item.status_id == 1 ||
+            //     item.status_id == 7 ||
+            //     item.status_id == 9 ||
+            //     item.status_id == 8
+            // ) {
+            //     request = {
+            //         campaign_user_id: null,
+            //         campaign_system_status_id: item.status_id,
+            //         end_time: null
+            //     };
+            // } else {
+            //     request = {
+            //         campaign_user_id: null
+            //     };
+            // }
+            // console.log(request);
+            // axios({
+            //     method: "PUT", //you can set what request you want to be
+            //     url:
+            //         "/api/removecharfromnode/" +
+            //         item.id +
+            //         "/" +
+            //         this.campaign_id,
+            //     data: request,
+            //     headers: {
+            //         Authorization: "Bearer " + this.$store.state.token,
+            //         Accept: "application/json",
+            //         "Content-Type": "application/json"
+            //     }
+            // });
+        }
+    },
 
     computed: {
-        ...mapGetters(["getNodeJoinByNode", "getNodeJoinByNodeCount"]),
+        ...mapGetters([
+            "getNodeJoinByNode",
+            "getNodeJoinByNodeCount",
+            "getCampaignUsersByUserIdEntosisCount"
+        ]),
 
         filteredItems() {
             console.log(this.sysid);
@@ -68,6 +243,12 @@ export default {
             } else {
                 return false;
             }
+        },
+
+        charCount() {
+            return this.getCampaignUsersByUserIdEntosisCount(
+                this.$store.state.user_id
+            );
         }
     }
 };
