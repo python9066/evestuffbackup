7d740d8802cc85d1027aa7da36c96821e1bc677e
python9066 <python9066@gmail.com>
2021-01-30 12:36:53 +0000

    quick commit

---
+++ resources/js/components/campaign/JoinNodeTable.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <v-btn
+            v-if="showAddIcon"
+            icon
+            color="light-blue darken-4"
+            @click="showAdd"
+        >
+            <v-icon small>fas fa-plus</v-icon></v-btn
+        >
+    </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import { mapState } from "vuex";
+export default {
+    props: {},
+    data() {
+        return {};
+    },
+
+    methods: {},
+
+    computed: {}
+};
+</script>
+
+<style></style>
