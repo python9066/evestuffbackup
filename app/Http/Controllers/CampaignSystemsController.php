3f6cb8f3629e24475a9f223751447bb4a1f045db
python9066 <python9066@gmail.com>
2021-02-07 23:50:35 +0000

    quick commit

--- app/Http/Controllers/CampaignSystemsController.php
+++ app/Http/Controllers/CampaignSystemsController.php
@@ -85,27 +85,27 @@ class CampaignSystemsController extends Controller
         }
         $pull = [];
         $nodeJoin = [];
-        $joins = NodeJoin::where('campaign_id', $campid)->get();
-        if ($joins->count() > 0) {
-            foreach ($joins as $join) {
+        // $joins = NodeJoin::where('campaign_id', $campid)->get();
+        // if ($joins->count() > 0) {
+        //     foreach ($joins as $join) {
 
-                $pull = [
-                    'id' => $join->id,
-                    'campaign_system_id' => $join->campaign_system_id,
-                    'campaign_user_id' => $join->campaign_user_id,
-                    'charname' => $join->campaignUser->char_name,
-                    'siteid' => $join->campaignUser->site_id,
-                    'mainname' => User::where('id', $join->campaignUser->site_id)->value('name'),
-                    'ship' => $join->campaignUser->ship,
-                    'link' => intval($join->campaignUser->link),
-                    'campaign_system_status_id' => intval($join->campaign_system_status_id),
-                    'statusName' => CampaignSystemStatus::where('id', $join->campaign_system_status_id)->value('name'),
-                    'campaign_sola_system_id' => CampaignSolaSystem::where('campaign_id', $campid)->where('system_id', $join->campaignSystem->system_id)->value('id'),
-                    'campaign_id' => $campid
-                ];
-                array_push($nodeJoin, $pull);
-            }
-        }
+        //         $pull = [
+        //             'id' => $join->id,
+        //             'campaign_system_id' => $join->campaign_system_id,
+        //             'campaign_user_id' => $join->campaign_user_id,
+        //             'charname' => $join->campaignUser->char_name,
+        //             'siteid' => $join->campaignUser->site_id,
+        //             'mainname' => User::where('id', $join->campaignUser->site_id)->value('name'),
+        //             'ship' => $join->campaignUser->ship,
+        //             'link' => intval($join->campaignUser->link),
+        //             'campaign_system_status_id' => intval($join->campaign_system_status_id),
+        //             'statusName' => CampaignSystemStatus::where('id', $join->campaign_system_status_id)->value('name'),
+        //             'campaign_sola_system_id' => CampaignSolaSystem::where('campaign_id', $campid)->where('system_id', $join->campaignSystem->system_id)->value('id'),
+        //             'campaign_id' => $campid
+        //         ];
+        //         array_push($nodeJoin, $pull);
+        //     }
+        // }
         $check = Auth::user();
         if ($check->hasPermissionTo('view_campaign_logs')) {
             $dataLog = [];
