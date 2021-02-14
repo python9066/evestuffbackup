923becf0d5a58f39f48e65f1bcc8e597708f8fd3
python9066 <python9066@gmail.com>
2021-02-14 22:49:32 +0000

    quick commit

--- database/migrations/2020_10_01_001625_create_stations_table.php
+++ database/migrations/2020_10_01_001625_create_stations_table.php
@@ -22,6 +22,28 @@ class CreateStationsTable extends Migration
             $table->integer('station_status_id')->default(1)->index();
             $table->foreignId('gunner_id')->nullable();
             $table->timestamp('out_time')->nullable();
+            $table->timestamp('repair_time')->nullable();
+            $table->dateTime('r_updated_at')->nullable();
+            $table->string('r_fitted')->nullable();
+            $table->integer('r_capital_shipyard', 1)->default(0);
+            $table->integer('r_hyasyoda', 1)->default(0);
+            $table->integer('r_invention', 1)->default(0);
+            $table->integer('r_manufacturing', 1)->default(0);
+            $table->integer('r_research', 1)->default(0);
+            $table->integer('r_supercapital_shipyard', 1)->default(0);
+            $table->integer('r_biochemical', 1)->default(0);
+            $table->integer('r_hybrid', 1)->default(0);
+            $table->integer('r_moon_drilling', 1)->default(0);
+            $table->integer('r_reprocessing', 1)->default(0);
+            $table->integer('r_point_defense', 1)->default(0);
+            $table->integer('r_dooms_day', 1)->default(0);
+            $table->integer('r_guide_bombs', 1)->default(0);
+            $table->integer('r_anti_cap', 1)->default(0);
+            $table->integer('r_anti_subcap', 1)->default(0);
+            $table->integer('r_t2_rigged', 1)->default(0);
+            $table->integer('r_cloning', 1)->default(0);
+            $table->integer('r_composite', 1)->default(0);
+            $table->timestamp('timestamp')->nullable();
             $table->timestamps();
         });
     }
