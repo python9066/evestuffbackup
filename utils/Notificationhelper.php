edfdc6305e0bf26edc4314371873a7bb42131fbb
python9066 <python9066@gmail.com>
2021-02-14 23:53:22 +0000

    quick commit

--- utils/Notificationhelper.php
+++ utils/Notificationhelper.php
@@ -50,7 +50,7 @@ class Notifications
     {
 
 
-        $stations = Station::where('id', '>', 0)->selete('id')->get();
+        $stations = Station::where('id', '>', 0)->value('id')->get();
         foreach ($stations as $station) {
             $url = "https://recon.gnf.lt/api/structure/" . $station;
             $client = new GuzzleHttpClient();
