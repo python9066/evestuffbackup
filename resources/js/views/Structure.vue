f7317100fe9abb418d221edec7e3dfcba6996dfe
python9066 <python9066@gmail.com>
2021-02-15 12:13:11 +0000

    quick commit

--- resources/js/views/Structure.vue
+++ resources/js/views/Structure.vue
@@ -52,7 +52,7 @@
             :footer-props="{ 'items-per-page-options': [15, 25, 50, 100, -1] }"
             :sort-by="['timestamp']"
             :search="search"
-            :sort-desc="[false, true]"
+            :sort-desc="[true, false]"
             multi-sort
             class="elevation-1"
         >
