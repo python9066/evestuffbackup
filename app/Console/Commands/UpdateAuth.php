f1b7410af9bfd99ab66e52b9a5b1f54508b85ad7
python9066 <python9066@gmail.com>
2021-02-15 14:03:07 +0000

    quick commit

---
+++ app/Console/Commands/UpdateAuth.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use utils\Helper\Helper;
+use utils\Timerhelper\Timerhelper;
+
+class UpdateAuth extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:auth';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'this will update structure vul windows';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Helper::authcheck();
+    }
+}
