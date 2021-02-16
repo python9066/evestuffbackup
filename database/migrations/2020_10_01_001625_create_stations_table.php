0deb91c212119c12d82a38f400cea55fb6d03113
python9066 <python9066@gmail.com>
2021-02-16 10:18:30 +0000

    quick commit

--- database/migrations/2020_10_01_001625_create_stations_table.php
+++ database/migrations/2020_10_01_001625_create_stations_table.php
@@ -43,6 +43,7 @@ class CreateStationsTable extends Migration
             $table->integer('r_t2_rigged', 1)->default(0);
             $table->integer('r_cloning', 1)->default(0);
             $table->integer('r_composite', 1)->default(0);
+            $table->timestamp('status_update')->nullable();
             $table->timestamp('timestamp')->nullable();
             $table->string('r_cored')->nullable();
             $table->timestamps();
