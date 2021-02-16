2adce0ce228ce97711946316e588a6da780b49f5
python9066 <python9066@gmail.com>
2021-02-16 16:08:46 +0000

    quick commit

--- resources/js/views/Structure.vue
+++ resources/js/views/Structure.vue
@@ -17,7 +17,7 @@
                 right-align
                 v-model="toggle_exclusive"
                 mandatory
-                :value="2"
+                value="1"
             >
                 <v-btn
                     :loading="loadingf"
