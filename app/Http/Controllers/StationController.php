2dc4ff09c39dc5bb41f7707a8c29c7155f4f6ad3
python9066 <python9066@gmail.com>
2021-02-18 00:09:03 +0000

    quick commit

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -81,7 +81,7 @@ class StationController extends Controller
         ]);
         broadcast(new StationCoreUpdate($flag));
         System::where('id', $request->system_id)->update(['task_flag' => 1]);
-        $url = "https://recon.gnf/api/task/add";
+        $url = "https://recon.gnf.lt/api/task/add";
         $client = new GuzzleHttpClient();
         $headers = [
             'x-gsf-user' => env('RECON_USER', 'DANCE2'),
