2c0e5a12ba6aa5a993220aacb55743a79cdf3950
python9066 <python9066@gmail.com>
2021-02-22 20:25:44 +0000

    quick commit

--- app/Http/Controllers/Auth/GiceProvider.php
+++ app/Http/Controllers/Auth/GiceProvider.php
@@ -71,6 +71,7 @@ class GiceProvider extends AbstractProvider implements ProviderInterface
         ]);
 
         // dd(json_decode($response->getBody(), true));
+        echo json_decode($response->getBody(), true);
         return json_decode($response->getBody(), true);
     }
 
