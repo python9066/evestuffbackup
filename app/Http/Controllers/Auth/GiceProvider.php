2f16e8f8e4440570de2782d307ceda1f9e2ec9b8
python9066 <python9066@gmail.com>
2021-02-22 19:51:36 +0000

    quick commit

--- app/Http/Controllers/Auth/GiceProvider.php
+++ app/Http/Controllers/Auth/GiceProvider.php
@@ -23,7 +23,7 @@ class GiceProvider extends AbstractProvider implements ProviderInterface
      */
     protected $scopes = [
         'openid',
-        // 'groups-limited'
+        'groups-limited'
     ];
 
     /**
@@ -133,7 +133,7 @@ class GiceProvider extends AbstractProvider implements ProviderInterface
             ],
         ]);
 
-        dd(json_decode($response->getBody(), true));
+        // dd(json_decode($response->getBody(), true));
 
         return json_decode($response->getBody(), true);
     }
