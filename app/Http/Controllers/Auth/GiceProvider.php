ec72efad3d63e528768e5e31c4b63c9b45ed83b3
python9066 <python9066@gmail.com>
2021-02-22 23:05:00 +0000

    quick commit

--- app/Http/Controllers/Auth/GiceProvider.php
+++ app/Http/Controllers/Auth/GiceProvider.php
@@ -24,7 +24,7 @@ class GiceProvider extends AbstractProvider implements ProviderInterface
      */
     protected $scopes = [
         'openid',
-        'groups-limited'
+        // 'groups-limited'
     ];
 
     /**
@@ -63,32 +63,32 @@ class GiceProvider extends AbstractProvider implements ProviderInterface
      * @param  string  $code
      * @return array
      */
-    // public function getAccessTokenResponse($code)
-    // {
-    //     $response = $this->getHttpClient()->post($this->getTokenUrl(), [
-    //         'headers' => ['Accept' => 'application/json'],
-    //         'form_params' => $this->getTokenFields($code),
-    //     ]);
-    //     return json_decode($response->getBody(), true);
-    // }
-
-
-    /////////
-
     public function getAccessTokenResponse($code)
     {
         $response = $this->getHttpClient()->post($this->getTokenUrl(), [
             'headers' => ['Accept' => 'application/json'],
             'form_params' => $this->getTokenFields($code),
         ]);
-        $arry = json_decode($response->getBody(), true);
-        $test = $arry['id_token'];
-        $info = explode(".", $test);
-        $information = base64_decode($info[1]);
-        dd($information);
-
         return json_decode($response->getBody(), true);
     }
+
+
+    /////////
+
+    // public function getAccessTokenResponse($code)
+    // {
+    //     $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+    //         'headers' => ['Accept' => 'application/json'],
+    //         'form_params' => $this->getTokenFields($code),
+    //     ]);
+    //     $arry = json_decode($response->getBody(), true);
+    //     $test = $arry['id_token'];
+    //     $info = explode(".", $test);
+    //     $information = base64_decode($info[1]);
+    //     dd($information);
+
+    //     return json_decode($response->getBody(), true);
+    // }
 
 
     ///////
