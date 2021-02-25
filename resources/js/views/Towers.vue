34cf13c3d1499ecc4a013fa03dc847367b712767
python9066 <python9066@gmail.com>
2021-02-25 02:09:54 +0000

    quick commit

--- resources/js/views/Towers.vue
+++ resources/js/views/Towers.vue
@@ -345,7 +345,7 @@ export default {
                     width: "5%"
                 },
                 { text: "System", value: "system_name", width: "5%" },
-                { text: "Alliance", value: "alliance_name", width: "15%" },
+                { text: "Alliance", value: "alliance_name", width: "12%" },
                 { text: "Moon", value: "moon_name", width: "8%" },
                 {
                     text: "Type",
