f609cfb72aeb624e1e0159ee8344db31baced803
python9066 <python9066@gmail.com>
2021-03-23 13:43:08 +0000

    quick commit

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -324,15 +324,17 @@ class StationController extends Controller
     public function update(Request $request, $id)
     {
 
-        $oldStatus = Station::where('id', $id)->pluck('station_status_id')->first();
+        $oldStatusID = Station::where('id', $id)->pluck('station_status_id')->first();
+        $oldStatusName = StationStatus::where('id', $oldStatusID)->pluck('name')->first();
+        $newStatusID = $request->station_status_id;
         Station::find($id)->update($request->all());
-        dd($oldStatus);
+        dd($oldStatusName);
         $message = StationRecords::where('id', $id)->first();
         $flag = collect([
             'message' => $message
         ]);
         broadcast(new StationNotificationUpdate($flag));
-        $text = Auth::user()->name . " Changed the status from " . $oldStatus . " to " . StationStatus::where($request->station_status_id, $oldStatus)->select('name')->get() . ' at ' . now();
+        $text = Auth::user()->name . " Changed the status from " . $oldStatus . " to " . StationStatus::where($request->, $oldStatus)->select('name')->get() . ' at ' . now();
         $logNew = Logging::Create(['structure_id' => $message->id, 'user_id' => Auth::id(), 'logging_type_id' => 18, 'text' => $text]);
     }
 
