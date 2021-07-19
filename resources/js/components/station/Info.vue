3e351e4e13c9c1ec7d1b1d618be6eb2e78f4d702
python9066 <python9066@gmail.com>
2021-07-19 23:31:01 +0100

    quick commit

--- resources/js/components/station/Info.vue
+++ resources/js/components/station/Info.vue
@@ -411,7 +411,7 @@ export default {
         },
         core() {
             var core = this.getCoreByStationID(this.station.id);
-            console.log(core[0]);
+            console.log(core[0].cored);
 
             if (this.fit == "NO") {
                 return "No Info";
