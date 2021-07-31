c975c74be08b9736b9780bd82e0c84924aaac626
python9066 <python9066@gmail.com>
2021-07-31 22:54:14 +0100

    quick commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -49,7 +49,6 @@ class AuthController extends Controller
         $this->purgeRoles($user);
         if (isset($userGice->grp)) {
             $roles = $userGice->grp;
-            dd($roles);
             if (is_array($roles)) {
 
                 foreach ($roles as $role) {
