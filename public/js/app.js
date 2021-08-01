16a1a32d1741fa3327fbbb6c71d9504094c968bb
python9066 <python9066@gmail.com>
2021-08-01 15:12:58 +0100

    quick commit

--- public/js/app.js
+++ public/js/app.js
@@ -36545,7 +36545,7 @@ var render = function() {
     _c("div", [
       _c(
         "span",
-        { staticClass: " d-inline-flex mb-4 pt-5" },
+        { staticClass: " d-inline-flex mb-4 pt-4" },
         [
           _c("VueCountUptimer", {
             attrs: {
