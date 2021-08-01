234f20511d2ff9711fe34525d6f4a63d50c2f56f
python9066 <python9066@gmail.com>
2021-08-01 12:52:44 +0100

    quick commit

--- app/Models/ReconTasks.php
+++ app/Models/ReconTasks.php
@@ -26,13 +26,10 @@ class ReconTasks extends Model
     }
 
     protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
         'id' => 'integer',
         'made_by_user_id' => 'integer',
         'edited_by_user_id' => 'integer'
-    ];
-
-    protected $dates = [
-        'created_at' => 'timestamp',
-        'updated_at' => 'timestamp',
     ];
 }
