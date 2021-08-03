85c99b11710f452ab16312b65ce66bee55512e38
python9066 <python9066@gmail.com>
2021-08-03 14:30:52 +0100

    quick commit

--- app/Http/Controllers/StartCampaignSystemController.php
+++ app/Http/Controllers/StartCampaignSystemController.php
@@ -56,7 +56,6 @@ class StartCampaignSystemController extends Controller
         if ($count != null) {
             CampaignUser::first('site_id', $request->user_id)->update(['campaign_id' => $id, 'campaign_system_id' => $request->sys]);
             $char = CampaignUser::where('site_id', $request->user_id)->first();
-            dd($char);
         } else {
             $char =  CampaignUser::create(['site_id' => Auth::id(), 'campaign_id' => $id, 'campaign_system_id' => $request->sys, 'char_name' => Auth::user()])->get();
         }
