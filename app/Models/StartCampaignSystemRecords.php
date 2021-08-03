c656e8ca74e056a5508b48a5d54e627734a6bfa7
python9066 <python9066@gmail.com>
2021-08-03 13:07:46 +0100

    quick commit

--- app/Models/StartCampaignSystemRecords.php
+++ app/Models/StartCampaignSystemRecords.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class StartCampaignSystemRecords extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'constellation_id' => 'integer',
+        'id' => 'integer',
+        'start_campaign_id' => 'integer',
+        'site_id' => 'integer',
+        'start_campaign_id' => 'integer',
+        'status_id' => 'integer',
+        'system_id' => 'integer',
+        'user_id' => 'integer'
+    ];
 }
