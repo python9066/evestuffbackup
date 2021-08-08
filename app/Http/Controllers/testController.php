b69784ef1637c56a4ea48658e41fce08b5f79f50
python9066 <python9066@gmail.com>
2021-08-09 00:50:14 +0100

    quick commit

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -33,7 +33,7 @@ class testController extends Controller
 
         $timers = $request;
         foreach ($timers as $timer) {
-            dd($timer, $request);
+            dd($timer, $request[0]);
         }
     }
 
