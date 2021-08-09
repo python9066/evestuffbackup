112afb3b0e506b32dd6e0b66077eeb34d56cec79
python9066 <python9066@gmail.com>
2021-08-09 02:55:36 +0100

    quick commit

--- app/Http/Controllers/RCSheet.php
+++ app/Http/Controllers/RCSheet.php
@@ -52,7 +52,7 @@ class RCSheet extends Controller
                 $itemID = Item::where('item_name', $input['Type'])->first();
                 $systemID = System::where('system_name', $input['System'])->first();
                 $new = Station::Create(['name' => $input['name'], 'system_id' => $systemID->id, 'alliance_id' => $allianceID->id, 'item_id' => $itemID->id, 'station_status_id' => $statusID, 'out_time' => $timer]);
-                // dd($allianceID->id, $input, $timer, $reconpull);
+                dd($allianceID->id, $input, $timer, $reconpull);
             }
         }
     }
