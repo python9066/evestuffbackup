6632e45a7830d4a58f4f080a132fa0c31a092346
python9066 <python9066@gmail.com>
2021-08-09 13:11:13 +0100

    quick commit

--- app/Models/RcStationRecords.php
+++ app/Models/RcStationRecords.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class RcStationRecords extends Model
 {
     use HasFactory;
+
+
+    protected $casts = [
+        'alliance_id' => 'integer',
+        'fc_user_id' => 'integer',
+        'item_id' => 'integer',
+        'rc_fc_id' => 'integer',
+        'rc_recon_id' => 'integer',
+        'recon_user_id' => 'integer',
+        'status_id' => 'integer',
+        'system_id' => 'integer'
+
+    ];
 }
