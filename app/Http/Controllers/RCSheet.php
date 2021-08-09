a14e7b6a3ccd9c84b664f56c5583323244b9d509
python9066 <python9066@gmail.com>
2021-08-09 09:21:39 +0100

    quick commit

--- app/Http/Controllers/RCSheet.php
+++ app/Http/Controllers/RCSheet.php
@@ -18,7 +18,11 @@ class RCSheet extends Controller
 {
     public function RCInput(Request $request)
     {
-        dd($request);
+        $inputs = $request->all();
+        foreach ($inputs as $input) {
+
+            dd($input);
+        }
         // $arry1 = (json_decode(utf8_encode($request), true));
         // $array = json_decode($request, TRUE);
         // dd($array, $arry1, $request[0], $request);
