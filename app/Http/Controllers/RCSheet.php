d82b25daa043783c6fd9b95a7a8a471f2758b5fd
python9066 <python9066@gmail.com>
2021-08-09 21:32:14 +0100

    quick commit

--- app/Http/Controllers/RCSheet.php
+++ app/Http/Controllers/RCSheet.php
@@ -100,7 +100,7 @@ class RCSheet extends Controller
                     }
 
 
-                    $check = Station::where('name', $input['structure_name'])->where('system_id', $input['solar_system']['solar_system_id'])->where('alliance_id', $allianceIDID)->get();
+                    $check = Station::where('name', "shit face")->where('system_id', $input['solar_system']['solar_system_id'])->where('alliance_id', $allianceIDID)->get();
                     dd($check, " this the count");
                     if ($check != null) {
                         // $checkid = $check["id"];
