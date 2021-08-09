283ddd36541fe23430eff1397eec440365959b11
python9066 <python9066@gmail.com>
2021-08-09 20:20:52 +0100

    quick commit

--- app/Http/Controllers/RCSheet.php
+++ app/Http/Controllers/RCSheet.php
@@ -102,10 +102,11 @@ class RCSheet extends Controller
                     if ($check) {
                         // $checkid = $check["id"];
                         if ($check->station_status_id == 5) {
+                            dd($check->station_status_id);
                             $statusID = 5;
                         }
                         $check->update(['station_status_id' => $statusID, 'out_time' => $timer]);
-                        if ($statusID = 5) {
+                        if ($statusID == 5) {
                             $check->update(['station_status_id' => $statusID, 'show_on_rc' => 2]);
                         }
                         // dd($check->id);
