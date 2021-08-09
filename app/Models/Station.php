829dbc8ff57bc792ff9eda1d170ed280d66ace74
python9066 <python9066@gmail.com>
2021-08-09 23:50:50 +0100

    quick commit

--- app/Models/Station.php
+++ app/Models/Station.php
@@ -29,4 +29,46 @@ class Station extends Model
     {
         return $this->belongsTo(Corp::class);
     }
+
+    protected $casts = [
+        'id' => 'integer',
+        'system_id' => 'integer',
+        'corp_id' => 'integer',
+        'alliance_id' => 'integer',
+        'item_id' => 'integer',
+        'user_id' => 'integer',
+        'station_status_id' => 'integer',
+        'gunner_id' => 'integer',
+        'ammo_request_id' => 'integer',
+        'r_capital_shipyard' => 'integer',
+        'r_hyasyoda' => 'integer',
+        'r_invention' => 'integer',
+        'r_manufacturing' => 'integer',
+        'r_research' => 'integer',
+        'r_supercapital_shipyard' => 'integer',
+        'r_biochemical' => 'integer',
+        'r_hybrid' => 'integer',
+        'r_moon_drilling' => 'integer',
+        'r_reprocessing' => 'integer',
+        'r_point_defense' => 'integer',
+        'r_dooms_day' => 'integer',
+        'r_anti_subcap' => 'integer',
+        'r_guide_bombs' => 'integer',
+        'r_anti_cap' => 'integer',
+        'r_t2_rigged' => 'integer',
+        'r_cloning' => 'integer',
+        'r_composite' => 'integer',
+        'r_cored' => 'integer',
+        'show_on_main' => 'integer',
+        'show_on_chill' => 'integer',
+        'show_on_rc' => 'integer',
+        'rc_fc_id' => 'integer',
+        'show_on_rc' => 'integer',
+        'rc_gsol_id' => 'integer',
+        'rc_recon_id' => 'integer',
+
+
+
+
+    ];
 }
