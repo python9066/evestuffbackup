b0d1fb7f6c3f8edbb7284e5240c203339850492b
python9066 <python9066@gmail.com>
2021-08-11 14:49:10 +0100

    quick commit

--- resources/js/components/station/AddStation.vue
+++ resources/js/components/station/AddStation.vue
@@ -536,19 +536,13 @@ export default {
             }
         },
         showSubmit() {
-            var d = parseInt(this.refTime.substr(0, 1));
-            var h = parseInt(this.refTime.substr(3, 2));
-            var m = parseInt(this.refTime.substr(6, 2));
-            var s = parseInt(this.refTime.substr(9, 2));
-            var timer = d + h + m + s;
-            console.log(timer);
             if (this.type == 3) {
                 if (
                     this.structSelect != null &&
                     this.sysSelect != null &&
                     this.tickSelect != null &&
                     this.refType != null &&
-                    this.refTime.length() == 0 &&
+                    this.refTime != null &&
                     this.imageLink != null
                 ) {
                     return false;
