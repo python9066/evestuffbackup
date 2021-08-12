29095bcfa06e46caf3225b61fce7a7ec32554cc6
python9066 <python9066@gmail.com>
2021-08-12 21:48:36 +0100

    quick commit

--- app/Http/Controllers/RCSheet.php
+++ app/Http/Controllers/RCSheet.php
@@ -21,10 +21,15 @@ class RCSheet extends Controller
     public function RCInput(Request $request)
     {
         $station = 1028507969993;
-        $item = 35921;
+        $items = StationItemJoin::where('station_id', $station)->get();
+        foreach ($items as $item) {
+            StationItemJoin::where('station_item_id', $item->station_item_id)->where('station_id', $station)->delete();
+            StationItemJoin::Create(['station_item_id' => $item, 'station_id' => $station]);
+        }
 
-        StationItemJoin::where('station_item_id', $item)->where('station_id', $station)->delete();
-        StationItemJoin::Create(['station_item_id' => $item, 'station_id' => $station]);
+
+
+
         dd("stop");
 
         Station::where('show_on_rc', 1)->update(['show_on_rc' => 5]);
