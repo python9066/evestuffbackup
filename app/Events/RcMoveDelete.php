3c4e16959eff364e2b26db089ade47dc7d3e83e3
python9066 <python9066@gmail.com>
2021-08-12 14:32:18 +0100

    quick commit

--- app/Events/RcMoveDelete.php
+++ app/Events/RcMoveDelete.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class RcMoveDelete
+class RcMoveDelete implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     public $flag;
