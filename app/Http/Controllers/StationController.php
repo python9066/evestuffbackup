cbd40331763f0c8f2b06a3787be5f25601d277d5
python9066 <python9066@gmail.com>
2021-08-13 13:09:12 +0100

    quick commit

--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -381,7 +381,7 @@ class StationController extends Controller
     public function editUpdate(Request $request, $id)
     {
         // dd($request);
-        Station::find($id)->update([$request->all()]);
+        Station::where('id', $id)->update([$request->all()]);
         $message = StationRecords::where('id', $id)->first();
         $flag = collect([
             'message' => $message
