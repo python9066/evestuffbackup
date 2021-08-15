7b7d83f466a17a33c6ebdb4ca38965845e3a5dc5
python9066 <python9066@gmail.com>
2021-08-15 08:52:41 +0100

    quick commit

--- resources/js/views/FleetKeyPannel.vue
+++ resources/js/views/FleetKeyPannel.vue
@@ -26,8 +26,8 @@
                         hide-details
                     ></v-text-field>
                 </v-card>
-                <EditKeys v-if="$can('edit_fleet_keys')"></EditKeys>
                 <EditFleets v-if="$can('edit_fleet_keys')"></EditFleets>
+                <EditKeys v-if="$can('edit_fleet_keys')"></EditKeys>
             </v-col>
         </v-row>
         <v-row no-gutters justify="center">
