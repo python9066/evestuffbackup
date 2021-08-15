d73dbe6890e7e7ca0ad2ced870164c2a654bed6c
python9066 <python9066@gmail.com>
2021-08-15 21:52:48 +0100

    quick commit

---
+++ app/Console/Commands/RunSchedulerDaemonCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RunSchedulerDaemonCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'schedule:daemon {--sleep=60}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Starts a daemon that will automatically run the Laravel schedule every 60 seconds';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle(): void
+    {
+        while (true) {
+            $this->call('schedule:run');
+            sleep($this->option('sleep'));
+        }
+    }
+}
