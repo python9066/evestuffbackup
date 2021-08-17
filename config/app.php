55bb7f698098ace39c7839ccdba2e57d06e17325
python9066 <python9066@gmail.com>
2021-08-17 01:59:58 +0100

    quick commit

--- config/app.php
+++ config/app.php
@@ -171,7 +171,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Onecentlin\Adminer\ServiceProvider::class,
+        // Onecentlin\Adminer\ServiceProvider::class,
 
         /*
          * Package Service Providers...
