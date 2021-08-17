53d92ab1d5d87940a2dc127fedd526f34ec1ffdf
python9066 <python9066@gmail.com>
2021-08-17 16:05:29 +0100

    quick commit

--- public/js/app.js
+++ public/js/app.js
@@ -25860,8 +25860,6 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 //
 //
 //
-//
-//
 
 
 
@@ -59581,7 +59579,7 @@ var render = function() {
                           _c(
                             "div",
                             { staticClass: " d-inline-flex" },
-                            _vm._l(_vm.fliterFleets(list.fleets), function(
+                            _vm._l(_vm.fliterFleets(list), function(
                               fleet,
                               index
                             ) {
