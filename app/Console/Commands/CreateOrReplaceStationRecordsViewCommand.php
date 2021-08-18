ad23d2ec1780358faad0679cafded942113c782a
python9066 <python9066@gmail.com>
2021-08-18 17:59:19 +0100

    quick commit

--- app/Console/Commands/CreateOrReplaceStationRecordsViewCommand.php
+++ app/Console/Commands/CreateOrReplaceStationRecordsViewCommand.php
@@ -72,6 +72,7 @@ class CreateOrReplaceStationRecordsViewCommand extends Command
        stations.show_on_main AS 'show_on_main',
        stations.show_on_chill AS 'show_on_chill',
        stations.show_on_rc_move AS 'show_on_rc_move',
+       stations.show_on_coord AS 'show_on_coord',
        if(stations.ammo_request_id IS NULL,0,1) AS 'ammo_request'
        FROM stations
        JOIN systems ON systems.id = stations.system_id
