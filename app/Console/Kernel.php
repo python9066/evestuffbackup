c0d8a9f66133ec6e1bb5eb2c4488ad6d4c22988a
python9066 <python9066@gmail.com>
2021-08-18 10:36:09 +0100

    quick commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,14 +29,14 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->everyMinute();
-        $schedule->command('update:campaigns')->everyMinute()->unlessBetween('11:00', '11:10')->withoutOverlapping();
-        $schedule->command('update:reconstations')->hourly()->withoutOverlapping();
-        $schedule->command('update:notifications')->everyMinute()->unlessBetween('11:00', '11:20')->withoutOverlapping();
-        $schedule->command('update:stationnotifications')->everyMinute()->withoutOverlapping();
-        $schedule->command('update:towers')->everyMinute()->withoutOverlapping();
-        $schedule->command('update:timers')->hourly()->unlessBetween('11:00', '11:20')->withoutOverlapping();
-        $schedule->command('update:alliances')->twiceDaily(9, 22)->withoutOverlapping();
-        $schedule->command('clear:remembertoken')->twiceDaily(9, 21)->withoutOverlapping();
+        // $schedule->command('update:campaigns')->everyMinute()->unlessBetween('11:00', '11:10')->withoutOverlapping();
+        // $schedule->command('update:reconstations')->hourly()->withoutOverlapping();
+        // $schedule->command('update:notifications')->everyMinute()->unlessBetween('11:00', '11:20')->withoutOverlapping();
+        // $schedule->command('update:stationnotifications')->everyMinute()->withoutOverlapping();
+        // $schedule->command('update:towers')->everyMinute()->withoutOverlapping();
+        // $schedule->command('update:timers')->hourly()->unlessBetween('11:00', '11:20')->withoutOverlapping();
+        // $schedule->command('update:alliances')->twiceDaily(9, 22)->withoutOverlapping();
+        // $schedule->command('clear:remembertoken')->twiceDaily(9, 21)->withoutOverlapping();
     }
 
     /**
