5436a8f85f0405e738d61a0f0bd98e9c8b3370c0
python9066 <python9066@gmail.com>
2021-08-19 23:58:55 +0100

    quick commit

--- utils/Notificationhelper.php
+++ utils/Notificationhelper.php
@@ -82,7 +82,7 @@ class Notifications
             StationItemJoin::where('station_id', $id)->delete();
         } else {
             StationItemJoin::where('station_id', $id)->delete();
-            Station::updateOrCreate(['id' => $id], [
+            $update = Station::updateOrCreate(['id' => $id], [
                 'name' => $stationdata['str_name'],
                 'r_hash' => $stationdata['str_structure_id_md5'],
                 'corp_id' => $stationdata['str_owner_corporation_id'],
@@ -109,9 +109,13 @@ class Notifications
                 'r_cored' => $stationdata['str_cored'],
                 'system_id' => $stationdata['str_system_id'],
                 'item_id' => $stationdata['str_type_id'],
-                'show_on_coord' => 1,
-                'station_status_id' => 1,
+
+
             ]);
+            if ($update->station_status_id == 1) {
+                $update->update(['show_on_coord' => 1]);
+            }
+
             if ($stationdata['str_has_no_fitting'] != null) {
                 if ($stationdata['str_has_no_fitting'] != 'No Fitting') {
                     StationItemJoin::where('station_id', $id)->delete();
