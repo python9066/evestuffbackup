60aa35291247ff75f749aceb05c7232c801b0c82
python9066 <python9066@gmail.com>
2021-08-19 01:24:02 +0100

    quick commit

--- utils/Campaignhelper.php
+++ utils/Campaignhelper.php
@@ -59,7 +59,7 @@ class Campaignhelper
     {
         $checkflag = null;
         //Removing old Campaigns and all data from databae -- start////
-        Userlogging::create('name', "demon");
+        Userlogging::create(['name' => "demon"]);
 
         $toDelete = Campaign::where('status_id', 10)
             ->get();
