54bd6ac207a4f9a6480cd666c66d5370c570233d
python9066 <python9066@gmail.com>
2021-08-20 02:14:43 +0100

    quick commit

--- utils/Notificationhelper.php
+++ utils/Notificationhelper.php
@@ -114,7 +114,8 @@ class Notifications
             $checkflag = Station::find($id)->first();
             if ($checkflag->show_on_rc != 1 || $checkflag->show_on_rc_move != 1) {
 
-                Station::where('id', $id)->update(['show_on_rc' => 1]);
+                Station::where('id', $id)->update(['show_on_coord' => 1]);
+
                 if ($checkflag->station_status_id == 10) {
                     Station::where('id', $id)->update(['station_status_id' => 1]);
                 }
