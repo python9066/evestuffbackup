a55d6816f140038fefe92ba2956c87aa189bcf5c
python9066 <python9066@gmail.com>
2021-08-21 12:24:03 +0100

    quick commit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -43,7 +43,8 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: '588e30b463a02a48ec1c',
+    // key: '588e30b463a02a48ec1c',
+    key: 'python9066',
     cluster: 'eu',
     wsHost: 'socket.evestuff.online',
     wsPort: 443,
