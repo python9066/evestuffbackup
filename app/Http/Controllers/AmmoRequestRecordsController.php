3e17fde4aa56bf21743e9dc982390197e02aa13a
python9066 <python9066@gmail.com>
2021-08-24 13:31:58 +0100

    quick commit

--- app/Http/Controllers/AmmoRequestRecordsController.php
+++ app/Http/Controllers/AmmoRequestRecordsController.php
@@ -19,7 +19,7 @@ class AmmoRequestRecordsController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newffffly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
