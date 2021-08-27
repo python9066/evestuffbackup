e788687256947db8e40bfcbb4a5fa9df139d73e5
python9066 <python9066@gmail.com>
2021-08-27 20:03:40 +0100

    quick commit

--- resources/js/components/chillstation/ChillStructure.vue
+++ resources/js/components/chillstation/ChillStructure.vue
@@ -809,7 +809,7 @@ export default {
             return this.$store.state.user_name;
         },
         height() {
-            let num = this.windowSize.y - 375;
+            let num = this.windowSize.y - 473;
             return num;
         }
     },
