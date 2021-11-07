c781b0efc2acc81f8015175da941a07210166c2f
python9066 <python9066@gmail.com>
2021-11-07 00:10:32 +0000

    quick commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Http\Controllers\Auth\GiceProvider;
 use Illuminate\Support\ServiceProvider;
+use Protoqol\Prequel\PrequelServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        if ($this->app->environment('local', 'develop', 'staging', 'production')) {
+            //$this->app->register(\Cuonggt\Dibi\DibiServiceProvider::class);
+            //$this->app->register(DibiServiceProvider::class);
+            //$this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            //$this->app->register(TelescopeServiceProvider::class);
+            $this->app->register(\Laravel\Horizon\HorizonServiceProvider::class);
+            $this->app->register(HorizonServiceProvider::class);
+            $this->app->register(\L5Swagger\L5SwaggerServiceProvider::class);
+            $this->app->register(PrequelServiceProvider::class);
+        }
     }
 
     /**
