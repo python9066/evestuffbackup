0060c5788db9cedf1d063e9be1b80c940428a2b9
python9066 <python9066@gmail.com>
2022-01-12 10:51:23 +0000

    quick commit

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ use Spatie\Permission\Models\Permission;
 Route::get('/jobtest', 'JobTestController@standingJob');
 Route::get('/dance', 'testController@corptest');
 Route::get('/testCorpWithNoAlliance', 'testController@getCorpWithNoAlliance');
-Route::get('/testCampagin' / 'testController@testPull');
+Route::get('/testCampagin', 'testController@testPull');
 Route::get('/73cbd63ecd4d2d9267ae4ad7bf25c704/5a1f48be9e4df773064f33590be892ff', 'AuthController@admin');
 Route::get('/73cbd63ecd4d2f33590be892ff', 'AuthController@webwayUser');
 Route::get('/7fegrghrthtrhtr2d9267ae4ad7bf25c704/5a1f48be9e4df773064f33590be892ff', 'AuthController@martyn');
