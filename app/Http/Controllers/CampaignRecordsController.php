76f1ae704535ec9e8d010cac4f1d9f8568384002
python9066 <python9066@gmail.com>
2022-04-13 21:15:51 +0100

    quick commit

--- app/Http/Controllers/CampaignRecordsController.php
+++ app/Http/Controllers/CampaignRecordsController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Events\CampaignSystemUpdate;
+use App\Models\Auth;
 use App\Models\Campaign;
 use App\Models\CampaignRecords;
 use Illuminate\Http\Request;
+use Spatie\Permission\Traits\HasRoles;
+use Spatie\Permission\Traits\HasPermissions;
 
 class CampaignRecordsController extends Controller
 {
+    use HasRoles;
+    use HasPermissions;
+
     /**
      * Display a listing of the resource.
      *
@@ -16,7 +22,16 @@ class CampaignRecordsController extends Controller
      */
     public function index()
     {
-        return ['campaigns' => CampaignRecords::all()];
+
+        if (Auth::user()->can("use_reserved_connection")) {
+            return ['campaigns' => CampaignRecords::with(['webway' => function ($t) {
+                $t->where('permissions', 1);
+            }])->get()];
+        } else {
+            return ['campaigns' => CampaignRecords::with(['webway' => function ($t) {
+                $t->where('permissions', 1);
+            }])->get()];
+        }
     }
 
     public function campaignslist()
