cc6d0f32b01d726ffc21a89b2bcb82ec2d76df13
python9066 <python9066@gmail.com>
2022-04-15 12:51:35 +0100

    quick commit

--- app/Models/NewCampaign.php
+++ app/Models/NewCampaign.php
@@ -9,4 +9,8 @@ class NewCampaign extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    public function fleets()
+    {
+        return $this->belongsToMany(FleetType::class, 'key_fleet_joins');
+    }
 }
