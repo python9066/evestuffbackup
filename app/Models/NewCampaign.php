b6397646d22d780c907fd89853ef2e5222edb678
python9066 <python9066@gmail.com>
2022-04-15 13:09:32 +0100

    quick commit

--- app/Models/NewCampaign.php
+++ app/Models/NewCampaign.php
@@ -9,8 +9,4 @@ class NewCampaign extends Model
 {
     use HasFactory;
     protected $guarded = [];
-    public function fleets()
-    {
-        return $this->belongsToMany(FleetType::class, 'key_fleet_joins');
-    }
 }
--- app/Models/NewOperation.php
+++ app/Models/NewOperation.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class NewOperation extends Model
 {
     use HasFactory;
+    protected $guarded = [];
 }
