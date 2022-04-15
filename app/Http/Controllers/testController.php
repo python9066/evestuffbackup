d3a92eb79fd52d95fa693a684d86950e85cd7164
python9066 <python9066@gmail.com>
2022-04-15 11:50:56 +0100

    quick commit

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -162,7 +162,7 @@ class testController extends Controller
         $campaigns = $response->collect();
         foreach ($campaigns as $campaign) {
 
-            dd($campaign->attackers_score);
+            dd($campaign['attackers_score']);
         }
     }
 
