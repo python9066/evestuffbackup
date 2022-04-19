ce3d85f6a9ab28adb6be7e782d4261df25c8049b
python9066 <python9066@gmail.com>
2022-04-19 10:56:57 +0100

    quick commit

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -136,6 +136,7 @@ class testController extends Controller
                     'campaign',
                     'campaign.constellation:id,constellation_name',
                     'campaign.alliance:id,name,ticker,standing,url,color',
+                    'campaign.system:id,system_name,adm',
                     'campaign.system.webway',
                     'campaign.structure:id,item_id,age',
                 ])
