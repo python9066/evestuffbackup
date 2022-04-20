1cc10d01dbc963e6ef35fa7da19473f9b2022e4a
python9066 <python9066@gmail.com>
2022-04-20 18:58:33 +0100

    quick commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,9 +28,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('update:campaigns')->everyMinute()->unlessBetween('11:00', '11:10')->withoutOverlapping();
-        $schedule->command('update:newCampaigns')->everyMinute()->unlessBetween('11:00', '11:10')->withoutOverlapping();
-        $schedule->command('update:eveusercount')->everyMinute()->unlessBetween('11:00', '11:10')->withoutOverlapping();
+        $schedule->command('update:campaigns')->everyMinute()->withoutOverlapping();
+        $schedule->command('update:newCampaigns')->everyMinute()->withoutOverlapping();
+        $schedule->command('update:eveusercount')->everyMinute()->withoutOverlapping();
         $schedule->command('clean:coordsheet')->everyMinute()->withoutOverlapping();
         $schedule->command('update:towers')->everyMinute()->withoutOverlapping();
         $schedule->command('update:notifications')->everyMinute()->unlessBetween('11:00', '11:20')->withoutOverlapping();
