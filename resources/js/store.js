9bc85a65003e952d3cde9e35f9627b375a0dc63c
python9066 <python9066@gmail.com>
2022-04-23 17:05:58 +0100

    quick commit

--- resources/js/store.js
+++ resources/js/store.js
@@ -2186,7 +2186,9 @@ export default new Vuex.Store({
         },
 
         getActiveRcStations: (state) => {
-            return state.stations.filter((station) => station.show_on_rc == 1);
+            return state.rcstations.filter(
+                (station) => station.show_on_rc == 1
+            );
         },
 
         getActiveChillStations: (state) => {
