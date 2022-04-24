27719ae481d630c044e7e65be33507897eba0f50
python9066 <python9066@gmail.com>
2022-04-24 10:25:09 +0100

    quick commit

---
+++ database/migrations/2022_04_24_090817_add_tactical_commander_role_and_permissions.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+
+class AddTacticalCommanderRoleAndPermissions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Artisan::call('permission:create-role "TC" web "access_campaigns"');
+        Artisan::call('permission:create-role "TC" web "edit_notifications"');
+        Artisan::call('permission:create-role "TC" web "view_killsheet"');
+        Artisan::call('permission:create-role "TC" web "view_move_timers"');
+        Artisan::call('permission:create-role "TC" web "view_station_list"');
+        Artisan::call('permission:create-role "TC" web "view_station_info_killsheet"');
+        Artisan::call('permission:create-role "FC" web "view_station_info_killsheet"');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
