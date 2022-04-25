b2a7daef41a96c070b1d0a40c11a1170ddd28614
python9066 <python9066@gmail.com>
2022-04-25 23:36:34 +0100

    quick commit

--- app/Http/Controllers/WebWayStartSystemsContorller.php
+++ app/Http/Controllers/WebWayStartSystemsContorller.php
@@ -66,7 +66,7 @@ class WebWayStartSystemsContorller extends Controller
         foreach ($system_ids as $pull) {
             array_push($ids, $pull['value']);
         }
-        dd(WebWay::whereNotIn('start_system_id', $ids)->get());
+        dd(["stations" => WebWay::whereNotIn('start_system_id', $ids)->get()]);
         WebWay::whereNotIn('start_system_id', $ids)->delete();
         WebWayStartSystem::whereNotIn('system_id', $ids)->delete();
         foreach ($ids as $system_id) {
