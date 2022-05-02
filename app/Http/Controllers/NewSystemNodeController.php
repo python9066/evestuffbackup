c4cf66ef28a54785db55b3cd92745bed4f797e77
python9066 <python9066@gmail.com>
2022-05-02 21:58:45 +0100

    quick commit

--- app/Http/Controllers/NewSystemNodeController.php
+++ app/Http/Controllers/NewSystemNodeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\NewCampaginOperation;
+use App\Models\NewCampaignSystem;
+use App\Models\NewOperation;
 use App\Models\NewSystemNode;
 use Illuminate\Http\Request;
 
@@ -26,6 +29,9 @@ class NewSystemNodeController extends Controller
     public function store(Request $request)
     {
         NewSystemNode::create($request->all());
+        $campaignIDs = NewCampaignSystem::where('system_id', $request->system_id)->select('campaign_id');
+        $obIDS = NewCampaginOperation::whereIn('campaign_id', $campaignIDs)->select('operation_id');
+        return $obIDS;
     }
 
     /**
