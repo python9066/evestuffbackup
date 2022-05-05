c6afb4e1a629f8453c1ca28b08ce2e54e1baacef
python9066 <python9066@gmail.com>
2022-05-05 01:37:44 +0100

    quick commit

--- resources/js/store.js
+++ resources/js/store.js
@@ -2498,7 +2498,7 @@ export default new Vuex.Store({
 
         getOwnHackingCharOnOp: (state) => (operationid) => {
             let pull = state.ownChars.filter(
-                (u) => u.role_id == 4 && u.operation_id == operationid
+                (u) => u.role_id != 4 && u.operation_id == operationid
             );
             let count = pull.length;
             if (count != 0) {
