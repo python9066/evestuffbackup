a46eb6918bfd5f79b8c441abc27caa1b00caf2b8
python9066 <python9066@gmail.com>
2022-05-17 09:53:01 +0100

    quick commit

---
+++ database/migrations/2022_05_17_085207_add_edit_priority_role.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+
+class AddEditPriorityRole extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Artisan::call('permission:create-role "Ops" web "edit_hack_priority"');
+        Artisan::call('permission:create-role "Coord" web "edit_hack_priority"');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
