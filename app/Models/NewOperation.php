46485c2596718f5cb808af2505c6d63270110623
python9066 <python9066@gmail.com>
2022-05-20 22:38:32 +0100

    quick commit

--- app/Models/NewOperation.php
+++ app/Models/NewOperation.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Activitylog\Traits\LogsActivity;
 
 
 class NewOperation extends Model
