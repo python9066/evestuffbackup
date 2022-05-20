88a91d06ea84190024e86a9bdd99ab292af00d08
python9066 <python9066@gmail.com>
2022-05-20 22:23:56 +0100

    quick commit

--- app/Models/NewOperation.php
+++ app/Models/NewOperation.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
 
 class NewOperation extends Model
 {
     use HasFactory;
+    use LogsActivity;
     protected $guarded = [];
 
     public function campaign()
