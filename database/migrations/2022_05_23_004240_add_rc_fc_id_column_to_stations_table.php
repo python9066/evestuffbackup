738a9500de21aeae80e9687d54b625c4f38ca011
python9066 <python9066@gmail.com>
2022-06-24 12:26:52 +0100

    quick commit

--- database/migrations/2022_05_23_004240_add_rc_fc_id_column_to_stations_table.php
+++ database/migrations/2022_05_23_004240_add_rc_fc_id_column_to_stations_table.php
@@ -14,7 +14,7 @@ class AddRcFcIdColumnToStationsTable extends Migration
     public function up()
     {
         Schema::table('stations', function (Blueprint $table) {
-           $table->foreignId('rc_fc_id')->nullable();
+            //    $table->foreignId('rc_fc_id')->nullable();
         });
     }
 
