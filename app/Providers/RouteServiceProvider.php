629617fbaedf7cabb98bbf79d7221ae25d1085bd
python9066 <python9066@gmail.com>
2022-06-24 21:30:58 +0100

    quick commit

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -14,7 +14,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    // protected $namespace = 'App\Http\Controllers';
+    protected $namespace = 'App\Http\Controllers';
 
     /**
      * The path to the "home" route for your application.
@@ -59,7 +59,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapWebRoutes()
     {
         Route::middleware('web')
-        // ->namespace($this->namespace)
+            ->namespace($this->namespace)
             ->group(base_path('routes/web.php'));
     }
 
@@ -74,7 +74,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::prefix('api')
             ->middleware('api')
-        // ->namespace($this->namespace)
+            ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
     }
 }
