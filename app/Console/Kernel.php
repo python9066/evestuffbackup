91a528ef1c1db6b1c41827fcc19101d15a740c76
python9066 <python9066@gmail.com>
2022-07-11 18:45:50 +0100

    fix timers being killed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,8 +30,8 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('update:EveEsiStatus')->everyMinute()->withoutOverlapping();
         $schedule->command('update:eveiduser')->everyMinute()->withoutOverlapping();
-        $schedule->command('update:campaigns')->everyMinute()->withoutOverlapping();
-        $schedule->command('update:campaginFix')->everyMinute()->withoutOverlapping();
+        // $schedule->command('update:campaigns')->everyMinute()->withoutOverlapping();
+        // $schedule->command('update:campaginFix')->everyMinute()->withoutOverlapping();
         $schedule->command('update:newCampaigns')->everyMinute()->withoutOverlapping();
         $schedule->command('update:eveusercount')->everyMinute()->withoutOverlapping();
         $schedule->command('update:opuserlist')->everyMinute();
