3e4d9b2dc916caa19e714b39df4782740ddfe0d3
python9066 <python9066@gmail.com>
2022-07-11 18:49:54 +0100

    fix timers being killed

--- resources/js/components/newcampaign/AddNode.vue
+++ resources/js/components/newcampaign/AddNode.vue
@@ -15,7 +15,7 @@
         @click="addShown = true"
         color="success"
         ><font-awesome-icon icon="fa-solid fa-plus" size="xl" pull="left" />
-        Node</v-btn
+        Nodee</v-btn
       >
     </template>
     <v-card :min-height="cardHight">
