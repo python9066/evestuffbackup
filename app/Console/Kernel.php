bcc5e963d8cd09564ceaaef4749ad1981284ac0c
python9066 <python9066@gmail.com>
2023-02-16 18:09:02 +0000

    foce

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,7 +33,7 @@ class Kernel extends ConsoleKernel
     ];
 
     /**
-     * Define the application's command schedule.ddddd
+     * Define the application's command schedule.ddddd update all the things
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
