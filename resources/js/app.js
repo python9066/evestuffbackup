1724384f2e0099cab5357f32c0822f35b24e7d75
Shift <shift@laravelshift.com>
2023-02-17 11:15:26 +0000

    Use ES modules

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require("./bootstrap");
+import "./bootstrap";
 // window.Vue = require('vue');
 import Vue from "vue";
 import Vuetify from "vuetify";
