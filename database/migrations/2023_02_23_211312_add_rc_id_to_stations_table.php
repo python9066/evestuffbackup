aeaa32d1241cfa39e1cfdcc62b65dfec74c2303a
python9066 <python9066@gmail.com>
2023-03-07 10:27:06 +0000

    fix migration

--- database/migrations/2023_02_23_211312_add_rc_id_to_stations_table.php
+++ database/migrations/2023_02_23_211312_add_rc_id_to_stations_table.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('stations', function (Blueprint $table) {
-            $table->foreignId('rc_id')->nullable();
+            // $table->foreignId('rc_id')->nullable();
         });
     }
 
