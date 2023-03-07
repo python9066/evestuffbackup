5d630ba647a4db1b86965bf7a0e3baf11a18842c
python9066 <python9066@gmail.com>
2023-03-07 10:49:58 +0000

    fix recon leader role

---
+++ database/migrations/2023_03_07_104802_update_recon_leader_role_id.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Role;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $role = Role::where('name', 'Recon Leader')->first();
+        $role->id = 14;
+        $role->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
